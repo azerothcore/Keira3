2677ed7afaefe70ef6a0977006a25e52f42f9ba0
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-10 00:04:35 +0200

    test travis

--- src/app/components/main-window/sidebar/sidebar.component.spec.ts
+++ src/app/components/main-window/sidebar/sidebar.component.spec.ts
@@ -1,13 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { instance, reset } from 'ts-mockito';
 
 import { SidebarComponent } from './sidebar.component';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { ElectronService } from '../../../services/electron.service';
-import { instance, reset } from 'ts-mockito';
 import { MockedElectronService, MockedMysqlService } from '../../../test-utils/mocks';
 import { MysqlService } from '../../../services/mysql.service';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
