7303f9f02e0055d34d241efcd0ece7bb93461810
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-10 19:24:19 +0200

    refactor(test): optmise module imports

--- src/app/components/editors/creature/creature-template-addon/creature-template-addon.component.spec.ts
+++ src/app/components/editors/creature/creature-template-addon/creature-template-addon.component.spec.ts
@@ -1,10 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { CreatureTemplateAddonComponent } from './creature-template-addon.component';
+import { CommonTestModule } from '../../../../test-utils/common-test.module';
 
 describe('CreatureTemplateAddonComponent', () => {
   let component: CreatureTemplateAddonComponent;
@@ -14,10 +12,7 @@ describe('CreatureTemplateAddonComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ CreatureTemplateAddonComponent ],
       imports: [
-        BrowserModule,
-        FormsModule,
-        ReactiveFormsModule,
-        RouterTestingModule,
+        CommonTestModule,
       ],
       schemas: [ NO_ERRORS_SCHEMA ],
     })
