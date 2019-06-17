f936c9dc2500cdde023b6135b23cc607ae8c0a94
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-17 23:55:29 +0200

    tests: fix build

--- src/app/services/search/item-search.service.spec.ts
+++ src/app/services/search/item-search.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { instance } from 'ts-mockito';
 
 import { ItemSearchService } from './item-search.service';
+import { QueryService } from '../query.service';
+import { MockedQueryService } from '../../test-utils/mocks';
 
 describe('ItemSearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: QueryService, useValue: instance(MockedQueryService) },
+    ]
+  }));
 
   it('should be created', () => {
     const service: ItemSearchService = TestBed.get(ItemSearchService);
