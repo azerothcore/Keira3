319ab1280f9b8e36d74eeeb2f386138ef761e5da
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-18 19:56:29 +0200

    feat: flags selector

--- src/app/components/editors/shared/selectors/flags-selector/flags-selector-modal.component.spec.ts
+++ src/app/components/editors/shared/selectors/flags-selector/flags-selector-modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 import { FlagsSelectorModalComponent } from './flags-selector-modal.component';
 import { CommonTestModule } from '../../../../../test-utils/common-test.module';
@@ -14,6 +15,7 @@ describe('FlagsSelectorModalComponent', () => {
       declarations: [ FlagsSelectorModalComponent ],
       imports: [
         CommonTestModule,
+        UiSwitchModule,
       ],
       providers: [
         BsModalRef,
---
+++ src/app/config/ui-switch.config.ts
@@ -0,0 +1,12 @@
+import { UiSwitchModuleConfig } from 'ngx-ui-switch/ui-switch/ui-switch.config';
+
+export const uiSwitchConfig: UiSwitchModuleConfig = {
+  size: 'small',
+  color: 'rgb(0, 189, 99)',
+  switchColor: '#FFF',
+  switchOffColor: '#FFF',
+  defaultBgColor: '#707375',
+  defaultBoColor : '#476EFF',
+  checkedLabel: '',
+  uncheckedLabel: '',
+};
