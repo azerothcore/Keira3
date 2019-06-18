65f3d4a2abcb76be37fdc8d0d24f284fd2bdc420
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-18 02:09:28 +0200

    feat: Item Selector

--- src/app/components/editors/shared/selectors/item-selector/item-selector-modal.component.spec.ts
+++ src/app/components/editors/shared/selectors/item-selector/item-selector-modal.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { BsModalRef } from 'ngx-bootstrap';
 import { instance } from 'ts-mockito';
 
 import { ItemSelectorModalComponent } from './item-selector-modal.component';
 import { QueryService } from '../../../../../services/query.service';
 import { MockedQueryService } from '../../../../../test-utils/mocks';
+import { CommonTestModule } from '../../../../../test-utils/common-test.module';
 
 describe('ItemSelectorModalComponent', () => {
   let component: ItemSelectorModalComponent;
@@ -13,10 +16,15 @@ describe('ItemSelectorModalComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ItemSelectorModalComponent ],
+      imports: [
+        CommonTestModule,
+        NgxDatatableModule,
+      ],
       providers: [
         BsModalRef,
         { provide: QueryService, useValue: instance(MockedQueryService) },
-      ]
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
