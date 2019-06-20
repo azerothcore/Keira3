54ce7a66ffd539e17e63eb50bda5c056d85dd42f
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-20 03:02:41 +0200

    feat: more selectors

--- src/app/components/editors/creature/spawns-addon/spawns-addon.component.ts
+++ src/app/components/editors/creature/spawns-addon/spawns-addon.component.ts
@@ -4,6 +4,7 @@ import { MultiRowEditorComponent } from '../../shared/multi-row-editor.component
 import { CreatureHandlerService } from '../../../../services/handlers/creature-handler.service';
 import { SpawnsAddon } from '../../../../types/spawns-addon.type';
 import { SpawnsAddonService } from '../../../../services/editors/creature/spawns-addon.service';
+import { EMOTE } from '../../../../constants/options/emote';
 
 @Component({
   selector: 'app-spawns-addon',
@@ -11,6 +12,8 @@ import { SpawnsAddonService } from '../../../../services/editors/creature/spawns
   styleUrls: ['./spawns-addon.component.scss']
 })
 export class SpawnsAddonComponent extends MultiRowEditorComponent<SpawnsAddon> {
+
+  public readonly EMOTE = EMOTE;
 
   constructor(
     public editorService: SpawnsAddonService,
