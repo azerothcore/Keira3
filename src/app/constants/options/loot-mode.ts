28dcf61f6121f5344639208d97fa64fac0c2f510
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-20 02:53:09 +0200

    fix(selectors): misc

--- src/app/constants/options/loot-mode.ts
+++
@@ -1,9 +0,0 @@
-import { Option } from '../../types/general';
-
-export const LOOT_MODE: Option[] = [
-  { value: 0, name: 'Default' },
-  { value: 1, name: 'HardMode1' },
-  { value: 2, name: 'HardMode2' },
-  { value: 3, name: 'HardMode3' },
-  { value: 4, name: 'HardMode4' },
-];
