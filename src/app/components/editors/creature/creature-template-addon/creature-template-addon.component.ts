fb75c06db71ecf412b88e141c07069f1c3050b64
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-21 12:13:49 +0200

    feat: tooltips/selectors to creature_template_addon

--- src/app/components/editors/creature/creature-template-addon/creature-template-addon.component.ts
+++ src/app/components/editors/creature/creature-template-addon/creature-template-addon.component.ts
@@ -5,6 +5,9 @@ import { CreatureHandlerService } from '../../../../services/handlers/creature-h
 import { CreatureTemplateAddon } from '../../../../types/creature-template-addon.type';
 import { CreatureTemplateAddonService } from '../../../../services/editors/creature/creature-template-addon.service';
 import { EMOTE } from '../../../../constants/options/emote';
+import { SPAWNS_ADDON_TABLE } from '../../../../types/spawns-addon.type';
+import { CREATURE_ADDON_BYTES_1 } from '../../../../constants/options/creature-addon-bytes1';
+import { CREATURE_ADDON_BYTES_2 } from '../../../../constants/options/creature-addon-bytes2';
 
 @Component({
   selector: 'app-creature-template-addon',
@@ -13,7 +16,13 @@ import { EMOTE } from '../../../../constants/options/emote';
 })
 export class CreatureTemplateAddonComponent extends SingleRowEditorComponent<CreatureTemplateAddon> {
 
+  public get docUrl() {
+    return this.WIKI_BASE_URL + SPAWNS_ADDON_TABLE; // they share the same doc page
+  }
+
   public readonly EMOTE = EMOTE;
+  public readonly CREATURE_ADDON_BYTES_1 = CREATURE_ADDON_BYTES_1;
+  public readonly CREATURE_ADDON_BYTES_2 = CREATURE_ADDON_BYTES_2;
 
   constructor(
     public editorService: CreatureTemplateAddonService,
