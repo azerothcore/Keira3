6fab14e70242e35a3212a12b3bfc7baeac9e7ccf
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-24 23:02:55 +0200

    refactor: prepare SpawnsAddonService logic for future reuse

---
+++ src/app/services/editors/multi-row-external-editor.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+import { MultiRowEditorService } from './multi-row-editor.service';
+import { Class, TableRow } from '../../types/general';
+import { HandlerService } from '../handlers/handler.service';
+import { QueryService } from '../query.service';
+
+// Used where none of the editor table's fields matches with the main entity ID. For example creature_addon (SpawnsAddonService)
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class MultiRowExternalEditorService<T extends TableRow> extends MultiRowEditorService<T> {
+
+  constructor(
+    protected _entityClass: Class,
+    protected _entityTable: string,
+    protected _entitySecondIdField: string,
+    protected handlerService: HandlerService<T>,
+    protected queryService: QueryService,
+  ) {
+    super(
+      _entityClass,
+      _entityTable,
+      null, // none of the editor table's fields matches with the main entity ID
+      _entitySecondIdField,
+      handlerService,
+      queryService,
+    );
+  }
+
+  disableEntityIdField() {}
+
+  protected updateFullQuery(): void {
+    this._fullQuery = this.queryService.getFullDeleteInsertQuery<T>(
+      this._entityTable,
+      this._newRows,
+      null,
+      this._entitySecondIdField,
+    );
+  }
+
+  protected updateDiffQuery(): void {
+    this._diffQuery = this.queryService.getDiffDeleteInsertTwoKeysQuery<T>(
+      this._entityTable,
+      null,
+      this._entitySecondIdField,
+      this._originalRows,
+      this._newRows,
+    );
+  }
+
+  abstract selectQuery(id: string|number);
+}
