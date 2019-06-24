e3d38796edcf6c5f0726fad007c2f79411492921
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-24 23:49:54 +0200

    fix build

--- src/app/services/editors/multi-row-external-editor.service.ts
+++ src/app/services/editors/multi-row-external-editor.service.ts
@@ -1,5 +1,3 @@
-import { Injectable } from '@angular/core';
-
 import { MultiRowEditorService } from './multi-row-editor.service';
 import { Class, TableRow } from '../../types/general';
 import { HandlerService } from '../handlers/handler.service';
@@ -7,9 +5,6 @@ import { QueryService } from '../query.service';
 
 // Used where none of the editor table's fields matches with the main entity ID. For example creature_addon (SpawnsAddonService)
 
-@Injectable({
-  providedIn: 'root'
-})
 export abstract class MultiRowExternalEditorService<T extends TableRow> extends MultiRowEditorService<T> {
 
   constructor(
