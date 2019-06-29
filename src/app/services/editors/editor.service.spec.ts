59698842c9962321d72335f1c1b987a3c7011dc5
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-30 01:09:45 +0200

    test: EditoService full coverage

--- src/app/services/editors/editor.service.spec.ts
+++ src/app/services/editors/editor.service.spec.ts
@@ -13,6 +13,8 @@ import { EditorService } from './editor.service';
 describe('EditorService', () => {
   let service: EditorService<MockEntity>;
 
+  const error = { code: 'some error', errno: 1234 } as MysqlError;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       RouterTestingModule,
@@ -36,13 +38,49 @@ describe('EditorService', () => {
 
   describe('reload', () => {
     let selectAllSpy: Spy;
+    let formResetSpy: Spy;
+    let onReloadSuccessfulSpy: Spy;
+
+    const id = 123456;
+    const data = 'mock data result';
 
     beforeEach(() => {
       selectAllSpy = spyOn(TestBed.get(QueryService), 'selectAll');
+      formResetSpy = spyOn(service.form, 'reset');
+      onReloadSuccessfulSpy = spyOn(TestBed.get(MockEditorService), 'onReloadSuccessful');
     });
 
-    it('TODO', () => {
+    it('should behave correctly when the query succeeds', () => {
+      service['_error'] = { code: 'some previous error', errno: 123 } as MysqlError;
+      service['_fullQuery'] = '-- some previous query';
+      service['_diffQuery'] = '-- some previous query';
+      selectAllSpy.and.returnValue(of(data));
 
+      service.reload(id);
+
+      expect(service.loading).toBe(false);
+      expect(service.error).toBe(null);
+      expect(service.fullQuery).toBe('');
+      expect(service.diffQuery).toBe('');
+      expect(selectAllSpy).toHaveBeenCalledWith(service.entityTable, service['_entityIdField'], id);
+      expect(formResetSpy).toHaveBeenCalledTimes(1);
+      expect(onReloadSuccessfulSpy).toHaveBeenCalledWith(data, id);
+    });
+
+    it('should behave correctly when the query errors', () => {
+      service['_fullQuery'] = '-- some previous query';
+      service['_diffQuery'] = '-- some previous query';
+      selectAllSpy.and.returnValue(throwError(error));
+
+      service.reload(id);
+
+      expect(service.loading).toBe(false);
+      expect(service.error).toEqual(error);
+      expect(service.fullQuery).toBe('');
+      expect(service.diffQuery).toBe('');
+      expect(selectAllSpy).toHaveBeenCalledWith(service.entityTable, service['_entityIdField'], id);
+      expect(formResetSpy).toHaveBeenCalledTimes(1);
+      expect(onReloadSuccessfulSpy).toHaveBeenCalledTimes(0);
     });
   });
 
@@ -62,7 +100,7 @@ describe('EditorService', () => {
 
       expect(querySpy).toHaveBeenCalledTimes(0);
       expect(reloadSpy).toHaveBeenCalledTimes(0);
-      expect(service['_loading']).toBe(false);
+      expect(service.loading).toBe(false);
     });
 
     it('should correctly work when the query succeeds', () => {
@@ -72,21 +110,20 @@ describe('EditorService', () => {
       service.save(query);
 
       expect(querySpy).toHaveBeenCalledWith(query);
-      expect(reloadSpy).toHaveBeenCalledWith(service['loadedEntityId']);
-      expect(service['_loading']).toBe(false);
-      expect(service['_error']).toBe(null);
+      expect(reloadSpy).toHaveBeenCalledWith(service.loadedEntityId);
+      expect(service.loading).toBe(false);
+      expect(service.error).toBe(null);
     });
 
     it('should correctly work when the query errors', () => {
-      const error = { code: 'some error', errno: 1234 } as MysqlError;
       querySpy.and.returnValue(throwError(error));
 
       service.save(query);
 
       expect(querySpy).toHaveBeenCalledWith(query);
       expect(reloadSpy).toHaveBeenCalledTimes(0);
-      expect(service['_loading']).toBe(false);
-      expect(service['_error']).toEqual(error);
+      expect(service.loading).toBe(false);
+      expect(service.error).toEqual(error);
     });
   });
 });
