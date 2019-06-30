2fd39d2d81cfc52bc57eb38b65c50e694f71dfed
FrancescoBorzi <borzifrancesco@gmail.com>
2019-06-30 20:28:55 +0200

    chore: more coverage

--- src/app/services/editors/single-row-editor.service.spec.ts
+++ src/app/services/editors/single-row-editor.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { instance } from 'ts-mockito';
+import Spy = jasmine.Spy;
 
 import { QueryService } from '../query.service';
 import { MockedQueryService } from '../../test-utils/mocks';
 import { CreatureTemplateService } from './creature/creature-template.service';
 import { SingleRowEditorService } from './single-row-editor.service';
-import { CreatureTemplate } from '../../types/creature-template.type';
+import { CREATURE_TEMPLATE_ID, CreatureTemplate } from '../../types/creature-template.type';
 
 
 describe('SingleRowEditorService', () => {
@@ -25,9 +26,51 @@ describe('SingleRowEditorService', () => {
     service = TestBed.get(CreatureTemplateService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
 
-  // TODO
+  describe('when the form value changes', () => {
+    let updateDiffQuerySpy: Spy;
+    let updateFullQuerySpy: Spy;
+
+    beforeEach(() => {
+      updateDiffQuerySpy = spyOn<any>(service, 'updateDiffQuery');
+      updateFullQuerySpy = spyOn<any>(service, 'updateFullQuery');
+    });
+
+    it('when loading is true, should do nothing', () => {
+      service['_loading'] = true;
+
+      service.form.get(CREATURE_TEMPLATE_ID).setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(0);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('when loading is false and the form is not dirty, should update only the full query', () => {
+      service.form.markAsPristine();
+
+      service.form.get(CREATURE_TEMPLATE_ID).setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(0);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('when loading is false and the form dirty, should update both the queries', () => {
+      service.form.markAsDirty();
+
+      service.form.get(CREATURE_TEMPLATE_ID).setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(1);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('modifying the form twice with the same value should not have effect', () => {
+      service.form.markAsDirty();
+
+      service.form.get(CREATURE_TEMPLATE_ID).setValue(123);
+      service.form.get(CREATURE_TEMPLATE_ID).setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(1);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+    });
+  });
 });
