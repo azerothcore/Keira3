6b4ca6594d82df5947173533846a2fe590cfb993
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-01 21:37:21 +0200

    chore: more coverage + small refactoring

--- src/app/services/editors/multi-row-editor.service.spec.ts
+++ src/app/services/editors/multi-row-editor.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { instance } from 'ts-mockito';
+import Spy = jasmine.Spy;
 
 import { QueryService } from '../query.service';
 import { MockedQueryService } from '../../test-utils/mocks';
 import { MultiRowEditorService } from './multi-row-editor.service';
-import { MockEntity, MockMultiRowEditorService } from '../../test-utils/mock-services';
+import { MOCK_ID, MOCK_ID_2, MOCK_NAME, MockEntity, MockMultiRowEditorService } from '../../test-utils/mock-services';
 
 
 describe('MultiRowEditorService', () => {
@@ -22,6 +23,53 @@ describe('MultiRowEditorService', () => {
 
   beforeEach(() => {
     service = TestBed.get(MockMultiRowEditorService);
+  });
+
+  describe('when the form value changes', () => {
+    let updateDiffQuerySpy: Spy;
+    let updateFullQuerySpy: Spy;
+
+    beforeEach(() => {
+      updateDiffQuerySpy = spyOn<any>(service, 'updateDiffQuery');
+      updateFullQuerySpy = spyOn<any>(service, 'updateFullQuery');
+    });
+
+    it('when loading is true, should do nothing', () => {
+      service['_loading'] = true;
+
+      service.form.get('id').setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(0);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('when isFormIdUnique() is false, should do nothing', () => {
+      spyOn(service, 'isFormIdUnique').and.returnValue(false);
+
+      service.form.get('id').setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(0);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('when loading is false and the form dirty, should update both the queries', () => {
+      service.form.markAsDirty();
+
+      service.form.get('id').setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(1);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('modifying the form twice with the same value should not have effect', () => {
+      service.form.markAsDirty();
+
+      service.form.get('id').setValue(123);
+      service.form.get('id').setValue(123);
+
+      expect(updateDiffQuerySpy).toHaveBeenCalledTimes(1);
+      expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+    });
   });
 
   it('updateDiffQuery() should correctly work', () => {
@@ -56,5 +104,43 @@ describe('MultiRowEditorService', () => {
       service['_entityIdField'],
     );
     expect(service.fullQuery).toEqual(queryResult);
+  });
+
+  describe('getRowIndex(id)', () => {
+    beforeEach(() => {
+      service['_newRows'] = [
+        { [MOCK_ID]: 123, [MOCK_ID_2]: 3, [MOCK_NAME]: 'test' },
+        { [MOCK_ID]: 123, [MOCK_ID_2]: 5, [MOCK_NAME]: 'test' },
+        { [MOCK_ID]: 123, [MOCK_ID_2]: 9, [MOCK_NAME]: 'test' },
+      ];
+    });
+
+    it('should correctly return the index', () => {
+      expect(service['getRowIndex'](3)).toEqual(0);
+      expect(service['getRowIndex'](5)).toEqual(1);
+      expect(service['getRowIndex'](9)).toEqual(2);
+    });
+
+    it('should give error if the index is not found', () => {
+      const errorSpy = spyOn(console, 'error');
+
+      service['getRowIndex'](133);
+
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('getSelectedRowIndex() should correctly work', () => {
+    const selectedRowId = 100;
+    const rowIndex = 3;
+    const getRowIndexSpy = spyOn<any>(service, 'getRowIndex').and.returnValue(rowIndex);
+    service['_selectedRowId'] = selectedRowId;
+
+    expect(service['getSelectedRowIndex']()).toEqual(rowIndex);
+    expect(getRowIndexSpy).toHaveBeenCalledWith(selectedRowId);
+  });
+
+  it('TODO', () => {
+
   });
 });
