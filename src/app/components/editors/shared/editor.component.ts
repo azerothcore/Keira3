d6d7b1a6b88292a1a768901028d2dea19f65a554
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-13 20:48:06 +0200

    refactor: avoid code duplication

--- src/app/components/editors/shared/editor.component.ts
+++ src/app/components/editors/shared/editor.component.ts
@@ -1,9 +1,11 @@
+import { OnInit } from '@angular/core';
+
 import { TableRow } from '../../../types/general';
 import { WIKI_BASE_URL } from '../../../constants/general';
 import { HandlerService } from '../../../services/handlers/handler.service';
 import { EditorService } from '../../../services/editors/editor.service';
 
-export abstract class EditorComponent<T extends TableRow> {
+export abstract class EditorComponent<T extends TableRow> implements OnInit {
   public readonly WIKI_BASE_URL = WIKI_BASE_URL;
   public get docUrl() {
     return this.WIKI_BASE_URL + this.editorService.entityTable;
@@ -13,4 +15,10 @@ export abstract class EditorComponent<T extends TableRow> {
     public editorService: EditorService<T>,
     protected handlerService: HandlerService<T>,
   ) { }
+
+  ngOnInit() {
+    if (this.editorService.loadedEntityId !== this.handlerService.selected) {
+      this.editorService.reload(this.handlerService.selected);
+    }
+  }
 }
--- src/app/components/editors/shared/single-row-editor.component.ts
+++ src/app/components/editors/shared/single-row-editor.component.ts
@@ -1,22 +1,15 @@
-import { OnInit } from '@angular/core';
-
 import { TableRow } from '../../../types/general';
 import { SingleRowEditorService } from '../../../services/editors/single-row-editor.service';
 import { HandlerService } from '../../../services/handlers/handler.service';
 import { EditorComponent } from './editor.component';
 
-export abstract class SingleRowEditorComponent<T extends TableRow> extends EditorComponent<T> implements OnInit {
+export abstract class SingleRowEditorComponent<T extends TableRow> extends EditorComponent<T> {
+
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
     public editorService: SingleRowEditorService<T>,
     protected handlerService: HandlerService<T>,
   ) {
     super(editorService, handlerService);
-  }
-
-  ngOnInit() {
-    if (this.editorService.loadedEntityId !== this.handlerService.selected) {
-      this.editorService.reload(this.handlerService.selected);
-    }
   }
 }
