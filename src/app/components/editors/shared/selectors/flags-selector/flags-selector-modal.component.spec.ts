7ff0fbb29e9c001036c314a939a750de4dfb291b
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-13 20:38:11 +0200

    test: more coverage

--- src/app/components/editors/shared/selectors/flags-selector/flags-selector-modal.component.spec.ts
+++ src/app/components/editors/shared/selectors/flags-selector/flags-selector-modal.component.spec.ts
@@ -4,10 +4,12 @@ import { UiSwitchModule } from 'ngx-ui-switch';
 
 import { FlagsSelectorModalComponent } from './flags-selector-modal.component';
 import { CommonTestModule } from '../../../../../test-utils/common-test.module';
+import { FlagsService } from '../../../../../services/helpers/flags.service';
 
 describe('FlagsSelectorModalComponent', () => {
   let component: FlagsSelectorModalComponent;
   let fixture: ComponentFixture<FlagsSelectorModalComponent>;
+  let flagsService: FlagsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,12 +26,42 @@ describe('FlagsSelectorModalComponent', () => {
   }));
 
   beforeEach(() => {
+    flagsService = TestBed.get(FlagsService);
+
     fixture = TestBed.createComponent(FlagsSelectorModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should safely create without config', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should properly handle config (if any)', () => {
+    const bits = [false, true, false];
+    const flags = 'mock flags';
+    const value = 123;
+    component.value = value;
+    component.config = { flags };
+    const getBitsArraySpy = spyOn(flagsService, 'getBitsArray').and.returnValue(bits);
+
+    component.ngOnInit();
+
+    expect(component.flagValues).toEqual(bits);
+    expect(getBitsArraySpy).toHaveBeenCalledTimes(1);
+    expect(getBitsArraySpy).toHaveBeenCalledWith(flags, value);
+  });
+
+  it('toogleBit() should properly work', () => {
+    const value = 123456;
+    const spyGetValueFromBits = spyOn(flagsService, 'getValueFromBits').and.returnValue(value);
+    component.flagValues = [true, false, true];
+
+    component.toggleBit(1);
+
+    expect(component.flagValues).toEqual([true, true, true]);
+    expect(spyGetValueFromBits).toHaveBeenCalledTimes(1);
+    expect(spyGetValueFromBits).toHaveBeenCalledWith([true, true, true]);
+    expect(component.value).toEqual(value);
   });
 });
