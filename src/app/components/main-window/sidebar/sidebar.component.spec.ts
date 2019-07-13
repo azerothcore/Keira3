ad88fd1d8029b6f47ec0066a74fe364e80681425
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-13 17:44:14 +0200

    test: minor improvements

--- src/app/components/main-window/sidebar/sidebar.component.spec.ts
+++ src/app/components/main-window/sidebar/sidebar.component.spec.ts
@@ -8,6 +8,14 @@ import { SidebarComponent } from './sidebar.component';
 import { ElectronService } from '../../../services/electron.service';
 import { MockedElectronService, MockedMysqlService } from '../../../test-utils/mocks';
 import { MysqlService } from '../../../services/mysql.service';
+import { PageObject } from '../../../test-utils/page-object';
+
+class SidebarComponentPage extends PageObject<SidebarComponent> {
+  get toggleSidebarBtn() { return this.query<HTMLButtonElement>('.sidebar-button'); }
+  get dashboardToggle() { return this.query<HTMLLIElement>('#dashboard-toggle'); }
+  get collapseAll() { return this.query<HTMLLIElement>('#collapse-all'); }
+  get creatureEditorToggle() { return this.query<HTMLLIElement>('#creature-editor-toggle'); }
+}
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
