be74bc4f07c0ead4da0e5fbe0a360c9b1d267f3b
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-13 20:48:06 +0200

    refactor: avoid code duplication

--- src/app/components/editors/shared/multi-row-editor.component.ts
+++ src/app/components/editors/shared/multi-row-editor.component.ts
@@ -1,12 +1,10 @@
-import { OnInit } from '@angular/core';
-
 import { TableRow } from '../../../types/general';
 import { HandlerService } from '../../../services/handlers/handler.service';
 import { EditorComponent } from './editor.component';
 import { MultiRowEditorService } from '../../../services/editors/multi-row-editor.service';
 import { DTCFG } from '../../../config/datatable.config';
 
-export abstract class MultiRowEditorComponent<T extends TableRow> extends EditorComponent<T> implements OnInit {
+export abstract class MultiRowEditorComponent<T extends TableRow> extends EditorComponent<T> {
 
   public readonly DTCFG = DTCFG;
 
@@ -16,11 +14,5 @@ export abstract class MultiRowEditorComponent<T extends TableRow> extends Editor
     protected handlerService: HandlerService<T>,
   ) {
     super(editorService, handlerService);
-  }
-
-  ngOnInit() {
-    if (this.editorService.loadedEntityId !== this.handlerService.selected) {
-      this.editorService.reload(this.handlerService.selected);
-    }
   }
 }
