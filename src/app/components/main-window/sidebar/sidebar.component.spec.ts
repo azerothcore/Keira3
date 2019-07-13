225cc6aadd74cdac9cadc320e6240a2c9d2e43d4
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-13 20:23:30 +0200

    test: more coverage

--- src/app/components/main-window/sidebar/sidebar.component.spec.ts
+++ src/app/components/main-window/sidebar/sidebar.component.spec.ts
@@ -9,17 +9,19 @@ import { ElectronService } from '../../../services/electron.service';
 import { MockedElectronService, MockedMysqlService } from '../../../test-utils/mocks';
 import { MysqlService } from '../../../services/mysql.service';
 import { PageObject } from '../../../test-utils/page-object';
+import { SidebarService } from './sidebar.service';
 
 class SidebarComponentPage extends PageObject<SidebarComponent> {
   get toggleSidebarBtn() { return this.query<HTMLButtonElement>('.sidebar-button'); }
-  get dashboardToggle() { return this.query<HTMLLIElement>('#dashboard-toggle'); }
-  get collapseAll() { return this.query<HTMLLIElement>('#collapse-all'); }
-  get creatureEditorToggle() { return this.query<HTMLLIElement>('#creature-editor-toggle'); }
+  get collapseAll() { return this.query<HTMLAnchorElement>('#collapse-all'); }
+  get creatureEditorToggle() { return this.query<HTMLAnchorElement>('#creature-editor-toggle'); }
 }
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let page: SidebarComponentPage;
+  let sidebarService: SidebarService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -40,13 +42,46 @@ describe('SidebarComponent', () => {
   }));
 
   beforeEach(() => {
+    sidebarService = TestBed.get(SidebarService);
+
     fixture = TestBed.createComponent(SidebarComponent);
+    page = new SidebarComponentPage(fixture);
     component = fixture.componentInstance;
+    fixture.autoDetectChanges(true);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('clicking the toggle button should correctly change the toggled status', () => {
+    sidebarService.setSidebarState(false);
+
+    page.clickElement(page.toggleSidebarBtn);
+    expect(sidebarService.getSidebarState()).toBe(true);
+
+    page.clickElement(page.toggleSidebarBtn);
+    expect(sidebarService.getSidebarState()).toBe(false);
+  });
+
+  it('toggling a section should correctly work', () => {
+    component.menuStates.creature = 'down';
+
+    page.clickElement(page.creatureEditorToggle);
+    expect(component.menuStates.creature).toBe('up');
+
+    page.clickElement(page.creatureEditorToggle);
+    expect(component.menuStates.creature).toBe('down');
+  });
+
+  it('collapse all button should correctly work ', () => {
+    component.menuStates.creature = 'down';
+    component.menuStates.quest = 'down';
+
+    page.clickElement(page.collapseAll);
+
+    for (const key in component.menuStates) {
+      if (component.menuStates.hasOwnProperty(key)) {
+        expect(component.menuStates[key]).toEqual('up');
+      }
+    }
   });
 
   afterEach(() => {
