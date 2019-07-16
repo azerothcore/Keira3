1ef7da3597038c049b139122aade03275ec7006f
FrancescoBorzi <borzifrancesco@gmail.com>
2019-07-16 22:54:38 +0200

    test: init SelectCreature integration test

--- src/app/components/editors/creature/select-creature/select-creature.integration.spec.ts
+++ src/app/components/editors/creature/select-creature/select-creature.integration.spec.ts
@@ -7,6 +7,21 @@ import { QueryService } from '../../../../services/query.service';
 import { SelectCreatureComponent } from './select-creature.component';
 import { CreatureSelectService } from '../../../../services/select/creature-select.service';
 import { SelectCreatureModule } from './select-creature.module';
+import { PageObject } from '../../../../test-utils/page-object';
+
+class SelectCreatureComponentPage extends PageObject<SelectCreatureComponent> {
+  get createInput() { return this.query<HTMLInputElement>('app-create input#id'); }
+  get selectNewBtn() { return this.query<HTMLButtonElement>('app-create #select-button'); }
+  get freeStatusWrapper() { return this.query<HTMLDivElement>('id-free-status'); }
+
+  get queryWrapper() { return this.query<HTMLElement>('code.hljs'); }
+
+  get searchEntryInput() { return this.query<HTMLInputElement>('input#entry'); }
+  get searchNameInput() { return this.query<HTMLInputElement>('input#name'); }
+  get searchSubnameInput() { return this.query<HTMLInputElement>('input#subname'); }
+  get searchLimitInput() { return this.query<HTMLInputElement>('input#limit'); }
+  get searchBtn() { return this.query<HTMLButtonElement>('#search-btn'); }
+}
 
 describe('SelectCreatureComponent', () => {
   let component: SelectCreatureComponent;
@@ -40,4 +55,6 @@ describe('SelectCreatureComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // TODO: test datatable selection
 });
