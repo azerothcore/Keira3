6cd80b637c46d0cbffbf45ab0b09c8450f35ff13
Francesco Borzì <borzifrancesco@gmail.com>
2019-07-18 22:44:08 +0200

    test: add Integration Tests for Select Creature feature (#5)

--- src/app/services/handlers/handler.service.ts
+++ src/app/services/handlers/handler.service.ts
@@ -1,6 +1,7 @@
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 
 import { TableRow } from '../../types/general';
+import { Observable } from 'rxjs';
 
 export abstract class HandlerService<T extends TableRow> implements CanActivate {
   private _selected: string;
