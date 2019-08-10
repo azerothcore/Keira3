5446cc8a1b35e39b150a9df6f76c659c0db1dd49
FrancescoBorzi <borzifrancesco@gmail.com>
2019-08-10 16:56:48 +0200

    refactor: reuse code

--- src/app/components/editors/quest/select-quest/select-quest.integration.spec.ts
+++ src/app/components/editors/quest/select-quest/select-quest.integration.spec.ts
@@ -8,44 +8,11 @@ import { QueryService } from '../../../../services/query.service';
 import { SelectQuestComponent } from './select-quest.component';
 import { QuestSelectService } from '../../../../services/select/quest-select.service';
 import { SelectQuestModule } from './select-quest.module';
-import { PageObject } from '../../../../test-utils/page-object';
 import { QuestTemplate } from '../../../../types/quest-template.type';
+import { SelectPageObject } from '../../../../test-utils/select-page-object';
 
-class SelectQuestComponentPage extends PageObject<SelectQuestComponent> {
-  get createInput() { return this.query<HTMLInputElement>('app-create input#id'); }
-  get selectNewBtn() { return this.query<HTMLButtonElement>('app-create #select-button'); }
-  get freeStatusWrapper() { return this.query<HTMLDivElement>('#id-free-status'); }
-
-  get queryWrapper() { return this.query<HTMLElement>('code.hljs'); }
-
-  get searchIdInput() { return this.query<HTMLInputElement>('input#search-id'); }
-  get searchNameInput() { return this.query<HTMLInputElement>('input#name'); }
-  get searchLimitInput() { return this.query<HTMLInputElement>('input#limit'); }
-  get searchBtn() { return this.query<HTMLButtonElement>('#search-btn'); }
-
-  get topBar() { return this.query<HTMLElement>('app-top-bar'); }
-
-  expectTopBarCreatingNew(id: number) {
-    expect(this.topBar.innerText).toContain(`Creating new: ${id}`);
-  }
-
-  expectTopBarEditing(id: number, name: string) {
-    expect(this.topBar.innerText).toContain(`Editing: ${name} (${id})`);
-  }
-
-  expectNewEntityFree() {
-    const error = 'Expected new entity to be free';
-    expect(this.selectNewBtn.disabled).toBe(false, error);
-    expect(this.freeStatusWrapper.innerHTML).toContain('fa-check-circle', error);
-    expect(this.freeStatusWrapper.innerText).toContain('The ID is free', error);
-  }
-
-  expectEntityAlreadyInUse() {
-    const error = 'Expected new entity to be already in use';
-    expect(this.selectNewBtn.disabled).toBe(true, error);
-    expect(this.freeStatusWrapper.innerHTML).toContain('fa-times-circle', error);
-    expect(this.freeStatusWrapper.innerText).toContain('The ID is already in use', error);
-  }
+class SelectQuestComponentPage extends SelectPageObject<SelectQuestComponent> {
+  ID_FIELD = 'ID';
 }
 
 describe('SelectQuest integration tests', () => {
