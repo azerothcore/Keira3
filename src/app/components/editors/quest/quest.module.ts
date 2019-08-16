0e04aba7b2ae3901aa48eb24867e42cb5fa7d63b
Francesco Borzì <borzifrancesco@gmail.com>
2019-08-17 00:53:34 +0200

    feat: QuestOfferReward editor (#45)

--- src/app/components/editors/quest/quest.module.ts
+++ src/app/components/editors/quest/quest.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { SelectQuestModule } from './select-quest/select-quest.module';
 import { QuestTemplateModule } from './quest-template/quest-template.module';
 import { QuestTemplateAddonModule } from './quest-template-addon/quest-template-addon.module';
+import { QuestOfferRewardModule } from './quest-offer-reward/quest-offer-reward.module';
 
 const modules = [
   SelectQuestModule,
   QuestTemplateModule,
   QuestTemplateAddonModule,
+  QuestOfferRewardModule,
 ];
 
 @NgModule({
