c68963d994b9187ae6a083fa8e007e6b7c4fd7d8
Helias <stefanoborzi32@gmail.com>
2019-08-17 10:42:37 +0200

    Added unit tests and select integration tests

--- src/app/components/editors/gameobject/select-gameobject/select-gameobject.integration.spec.ts
+++ src/app/components/editors/gameobject/select-gameobject/select-gameobject.integration.spec.ts
@@ -9,6 +9,7 @@ import { SelectGameobjectComponent } from './select-gameobject.component';
 import { GameobjectSelectService } from '../../../../services/select/gameobject-select.service';
 import { SelectGameobjectModule } from './select-gameobject.module';
 import { SelectPageObject } from '../../../../test-utils/select-page-object';
+import { GameobjectTemplate } from '../../../../types/gameobject-template.type';
 
 class SelectGameobjectComponentPage extends SelectPageObject<SelectGameobjectComponent> {
   ID_FIELD = 'entry';
@@ -104,66 +105,66 @@ describe('SelectGameobject integration tests', () => {
     });
   }));
 
-  // for (const { testId, id, name, limit, expectedQuery } of [
-  //   {
-  //     testId: 1, id: 1200, name: `The People's Militia`, limit: '100', expectedQuery:
-  //       'SELECT * FROM `gameobject_template` WHERE (`entry` LIKE \'%1200%\') AND (`name` LIKE \'%The People\\\'s Militia%\') LIMIT 100'
-  //   },
-  //   {
-  //     testId: 2, id: '', name: `The People's Militia`, limit: '100', expectedQuery:
-  //       'SELECT * FROM `gameobject_template` WHERE (`name` LIKE \'%The People\\\'s Militia%\') LIMIT 100'
-  //   },
-  //   {
-  //     testId: 3, id: '', name: `The People's Militia`, limit: '100', expectedQuery:
-  //       'SELECT * FROM `gameobject_template` WHERE (`name` LIKE \'%The People\\\'s Militia%\') LIMIT 100'
-  //   },
-  //   {
-  //     testId: 4, id: 1200, name: '', limit: '', expectedQuery:
-  //       'SELECT * FROM `gameobject_template` WHERE (`entry` LIKE \'%1200%\')'
-  //   },
-  // ]) {
-  //   it(`searching an existing entity should correctly work [${testId}]`, () => {
-  //     querySpy.calls.reset();
-  //     if (id) {
-  //       page.setInputValue(page.searchIdInput, id);
-  //     }
-  //     if (name) {
-  //       page.setInputValue(page.searchNameInput, name);
-  //     }
-  //     page.setInputValue(page.searchLimitInput, limit);
+  for (const { testId, id, name, limit, expectedQuery } of [
+    {
+      testId: 1, id: 1200, name: `Solid Chest`, limit: '100', expectedQuery:
+        'SELECT * FROM `gameobject_template` WHERE (`entry` LIKE \'%1200%\') AND (`name` LIKE \'%Solid Chest%\') LIMIT 100'
+    },
+    {
+      testId: 2, id: '', name: `Solid Chest`, limit: '100', expectedQuery:
+        'SELECT * FROM `gameobject_template` WHERE (`name` LIKE \'%Solid Chest%\') LIMIT 100'
+    },
+    {
+      testId: 3, id: '', name: `Solid Chest`, limit: '100', expectedQuery:
+        'SELECT * FROM `gameobject_template` WHERE (`name` LIKE \'%Solid Chest%\') LIMIT 100'
+    },
+    {
+      testId: 4, id: 1200, name: '', limit: '', expectedQuery:
+        'SELECT * FROM `gameobject_template` WHERE (`entry` LIKE \'%1200%\')'
+    },
+  ]) {
+    it(`searching an existing entity should correctly work [${testId}]`, () => {
+      querySpy.calls.reset();
+      if (id) {
+        page.setInputValue(page.searchIdInput, id);
+      }
+      if (name) {
+        page.setInputValue(page.searchNameInput, name);
+      }
+      page.setInputValue(page.searchLimitInput, limit);
 
-  //     expect(page.queryWrapper.innerText).toContain(expectedQuery);
+      expect(page.queryWrapper.innerText).toContain(expectedQuery);
 
-  //     page.clickElement(page.searchBtn);
+      page.clickElement(page.searchBtn);
 
-  //     expect(querySpy).toHaveBeenCalledTimes(1);
-  //     expect(querySpy).toHaveBeenCalledWith(expectedQuery);
-  //   });
-  // }
+      expect(querySpy).toHaveBeenCalledTimes(1);
+      expect(querySpy).toHaveBeenCalledWith(expectedQuery);
+    });
+  }
 
-  // it('searching and selecting an existing entity from the datatable should correctly work', () => {
-  //   const results: Partial<GameobjectTemplate>[] = [
-  //     { id: 1, name: 'An awesome Gameobject 1', GameobjectType: 0, GameobjectLevel: 1, MinLevel: 10, GameobjectDescription: ''   },
-  //     { id: 2, name: 'An awesome Gameobject 2', GameobjectType: 0, GameobjectLevel: 2, MinLevel: 20, GameobjectDescription: ''   },
-  //     { id: 3, name: 'An awesome Gameobject 3', GameobjectType: 0, GameobjectLevel: 3, MinLevel: 30, GameobjectDescription: ''   },
-  //   ];
-  //   querySpy.calls.reset();
-  //   querySpy.and.returnValue(of({ results }));
+  it('searching and selecting an existing entity from the datatable should correctly work', () => {
+    const results: Partial<GameobjectTemplate>[] = [
+      { id: 1, name: 'An awesome Gameobject 1', GameobjectType: 0, GameobjectLevel: 1, MinLevel: 10, GameobjectDescription: ''   },
+      { id: 2, name: 'An awesome Gameobject 2', GameobjectType: 0, GameobjectLevel: 2, MinLevel: 20, GameobjectDescription: ''   },
+      { id: 3, name: 'An awesome Gameobject 3', GameobjectType: 0, GameobjectLevel: 3, MinLevel: 30, GameobjectDescription: ''   },
+    ];
+    querySpy.calls.reset();
+    querySpy.and.returnValue(of({ results }));
 
-  //   page.clickElement(page.searchBtn);
+    page.clickElement(page.searchBtn);
 
-  //   const row0 = page.getDatatableRow(page.DT_SELECTOR, 0);
-  //   const row1 = page.getDatatableRow(page.DT_SELECTOR, 1);
-  //   const row2 = page.getDatatableRow(page.DT_SELECTOR, 2);
+    const row0 = page.getDatatableRow(page.DT_SELECTOR, 0);
+    const row1 = page.getDatatableRow(page.DT_SELECTOR, 1);
+    const row2 = page.getDatatableRow(page.DT_SELECTOR, 2);
 
-  //   expect(row0.innerText).toContain(results[0].name);
-  //   expect(row1.innerText).toContain(results[1].name);
-  //   expect(row2.innerText).toContain(results[2].name);
+    expect(row0.innerText).toContain(results[0].name);
+    expect(row1.innerText).toContain(results[1].name);
+    expect(row2.innerText).toContain(results[2].name);
 
-  //   page.clickElement(page.getDatatableCell(page.DT_SELECTOR, 1, 1));
+    page.clickElement(page.getDatatableCell(page.DT_SELECTOR, 1, 1));
 
-  //   expect(navigateSpy).toHaveBeenCalledTimes(1);
-  //   expect(navigateSpy).toHaveBeenCalledWith(['gameobject/gameobject-template']);
-  //   page.expectTopBarEditing(results[1].entry, results[1].name);
-  // });
+    expect(navigateSpy).toHaveBeenCalledTimes(1);
+    expect(navigateSpy).toHaveBeenCalledWith(['gameobject/gameobject-template']);
+    page.expectTopBarEditing(results[1].entry, results[1].name);
+  });
 });
--- src/app/services/editors/gameobject/gameobject-template.service.spec.ts
+++ src/app/services/editors/gameobject/gameobject-template.service.spec.ts
@@ -5,8 +5,10 @@ import { instance } from 'ts-mockito';
 import { GameobjectTemplateService } from './gameobject-template.service';
 import { QueryService } from '../../query.service';
 import { MockedQueryService } from '../../../test-utils/mocks';
+import { GO_DATA_FIELDS } from '../../../constants/gameobject-types';
 
 describe('GameobjectTemplateService', () => {
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       RouterTestingModule,
@@ -17,7 +19,14 @@ describe('GameobjectTemplateService', () => {
   }));
 
   it('should be created', () => {
-    const service: GameobjectTemplateService = TestBed.get(GameobjectTemplateService);
-    expect(service).toBeTruthy();
+    const gameobjectService: GameobjectTemplateService = TestBed.get(GameobjectTemplateService);
+    expect(gameobjectService).toBeTruthy();
   });
+
+  it('changing type and the related fields data*', () => {
+    const gameobjectService: GameobjectTemplateService = TestBed.get(GameobjectTemplateService);
+    expect(gameobjectService.getFieldDefinition(0, 0)).toBe(GO_DATA_FIELDS[0][0]);
+    expect(gameobjectService.getFieldDefinition(0, 25)).toEqual({ name: `Data25`, tooltip: null });
+  });
+
 });
