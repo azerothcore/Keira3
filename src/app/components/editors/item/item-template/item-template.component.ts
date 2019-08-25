49db2ef939f373e6904203853ee0b8393f473b10
FrancescoBorzi <borzifrancesco@gmail.com>
2019-08-25 23:03:51 +0200

    wip

--- src/app/components/editors/item/item-template/item-template.component.ts
+++ src/app/components/editors/item/item-template/item-template.component.ts
@@ -8,6 +8,9 @@ import { ITEM_CLASS, ITEM_SUBCLASS } from '../../../../constants/options/item-cl
 import { ITEM_QUALITY } from '../../../../constants/options/item-quality';
 import { ITEM_FLAGS } from '../../../../constants/flags/item-flags';
 import { ITEM_FLAGS_EXTRA } from '../../../../constants/flags/item-flags-extra';
+import { INVENTORY_TYPE } from '../../../../constants/options/inventory-type';
+import { ALLOWABLE_CLASSES } from '../../../../constants/flags/allowable-classes';
+import { ALLOWABLE_RACES } from '../../../../constants/flags/allowable-races';
 
 @Component({
   selector: 'app-item-template',
@@ -21,6 +24,9 @@ export class ItemTemplateComponent extends SingleRowEditorComponent<ItemTemplate
   public readonly ITEM_QUALITY = ITEM_QUALITY;
   public readonly ITEM_FLAGS = ITEM_FLAGS;
   public readonly ITEM_FLAGS_EXTRA = ITEM_FLAGS_EXTRA;
+  public readonly INVENTORY_TYPE = INVENTORY_TYPE;
+  public readonly ALLOWABLE_CLASSES = ALLOWABLE_CLASSES;
+  public readonly ALLOWABLE_RACES = ALLOWABLE_RACES;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
