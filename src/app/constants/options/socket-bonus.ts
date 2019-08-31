a90eae1f611b6d56c07230f73a262abcf6b8ca34
FrancescoBorzi <borzifrancesco@gmail.com>
2019-08-31 13:23:05 +0200

    chore: add more options and flags for item-template

---
+++ src/app/constants/options/socket-bonus.ts
@@ -0,0 +1,12 @@
+import { Option } from '../../types/general';
+
+export const SOCKET_BONUS: Option[] = [
+  { value: 3312, name: '+8 Strength'},
+  { value: 3313, name: '+8 Agility'},
+  { value: 3305, name: '+12 Stamina'},
+  { value: 3, name: '+8 Intellect'},
+  { value: 2872, name: '+9 Healing'},
+  { value: 3753, name: '+9 Spell Power'},
+  { value: 3877, name: '+16 Attack Power'},
+  // TODO: add more options. The current ones are just some commonly used socket bonuse, not a complete list
+];
