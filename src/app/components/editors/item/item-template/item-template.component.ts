b6aa7d9af4f8f4170f041aca6726c6a97864a0aa
FrancescoBorzi <borzifrancesco@gmail.com>
2019-08-31 22:39:39 +0200

    chore: add selectors in html

--- src/app/components/editors/item/item-template/item-template.component.ts
+++ src/app/components/editors/item/item-template/item-template.component.ts
@@ -12,6 +12,14 @@ import { INVENTORY_TYPE } from '../../../../constants/options/inventory-type';
 import { ALLOWABLE_CLASSES } from '../../../../constants/flags/allowable-classes';
 import { ALLOWABLE_RACES } from '../../../../constants/flags/allowable-races';
 import { FACTION_RANK } from '../../../../constants/options/faction-rank';
+import { BAG_FAMILY } from '../../../../constants/flags/bag-family';
+import { SOCKET_COLOR } from '../../../../constants/flags/socket-color';
+import { ITEM_BONDING } from '../../../../constants/options/item-bonding';
+import { ITEM_MATERIAL } from '../../../../constants/options/item-material';
+import { ITEM_SHEAT } from '../../../../constants/options/item-sheath';
+import { TOTEM_CATEGORY } from '../../../../constants/options/totem-category';
+import { FOOD_TYPE } from '../../../../constants/options/foot-type';
+import { ITEM_FLAGS_CUSTOM } from '../../../../constants/flags/item-flags-custom';
 
 @Component({
   selector: 'app-item-template',
@@ -29,6 +37,14 @@ export class ItemTemplateComponent extends SingleRowEditorComponent<ItemTemplate
   public readonly ALLOWABLE_CLASSES = ALLOWABLE_CLASSES;
   public readonly ALLOWABLE_RACES = ALLOWABLE_RACES;
   public readonly FACTION_RANK = FACTION_RANK;
+  public readonly BAG_FAMILY = BAG_FAMILY;
+  public readonly SOCKET_COLOR = SOCKET_COLOR;
+  public readonly ITEM_BONDING = ITEM_BONDING;
+  public readonly ITEM_MATERIAL = ITEM_MATERIAL;
+  public readonly ITEM_SHEAT = ITEM_SHEAT;
+  public readonly TOTEM_CATEGORY = TOTEM_CATEGORY;
+  public readonly FOOD_TYPE = FOOD_TYPE;
+  public readonly ITEM_FLAGS_CUSTOM = ITEM_FLAGS_CUSTOM;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
