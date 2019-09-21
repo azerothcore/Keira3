22993afd607d82dedf6a74e7b86144c91e263f73
Francesco Borzì <borzifrancesco@gmail.com>
2019-09-21 18:19:02 +0200

    chore: add types and skeleton modules for conditions & smart-ai (#121)

---
+++ src/app/types/smart-scripts.type.ts
@@ -0,0 +1,34 @@
+import { TableRow } from './general';
+
+export class SmartScripts extends TableRow {
+  entryorguid: number = 0;
+  source_type: number = 0;
+  id: number = 0;
+  link: number = 0;
+  event_type: number = 0;
+  event_phase_mask: number = 0;
+  event_chance: number = 100;
+  event_flags: number = 0;
+  event_param1: number = 0;
+  event_param2: number = 0;
+  event_param3: number = 0;
+  event_param4: number = 0;
+  event_param5: number = 0;
+  action_type: number = 0;
+  action_param1: number = 0;
+  action_param2: number = 0;
+  action_param3: number = 0;
+  action_param4: number = 0;
+  action_param5: number = 0;
+  action_param6: number = 0;
+  target_type: number = 0;
+  target_param1: number = 0;
+  target_param2: number = 0;
+  target_param3: number = 0;
+  target_param4: number = 0;
+  target_x: number = 0;
+  target_y: number = 0;
+  target_z: number = 0;
+  target_o: number = 0;
+  comment: string = '';
+}
