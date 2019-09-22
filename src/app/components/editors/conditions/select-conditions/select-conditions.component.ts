79f5257700590f266a194b01826aab99fe521900
FrancescoBorzi <borzifrancesco@gmail.com>
2019-09-22 13:36:51 +0200

    chore: wip

--- src/app/components/editors/conditions/select-conditions/select-conditions.component.ts
+++ src/app/components/editors/conditions/select-conditions/select-conditions.component.ts
@@ -3,6 +3,7 @@ import { Component, } from '@angular/core';
 import { QueryService } from '../../../../services/query.service';
 import { DTCFG } from '../../../../config/datatable.config';
 import { ConditionsSearchService } from '../../../../services/search/conditions-search.service';
+import { CONDITION_SOURCE_TYPES, CONDITION_SOURCE_TYPES_KEYS } from '../../../../types/conditions.type';
 
 @Component({
   selector: 'app-select-creature',
@@ -11,6 +12,8 @@ import { ConditionsSearchService } from '../../../../services/search/conditions-
 })
 export class SelectConditionsComponent {
   public readonly DTCFG = DTCFG;
+  public readonly CONDITION_SOURCE_TYPES = CONDITION_SOURCE_TYPES;
+  public readonly CONDITION_SOURCE_TYPES_KEYS = CONDITION_SOURCE_TYPES_KEYS;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
