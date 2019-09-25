8699681a5accc4c9e505378ce766ffe46673c91c
FrancescoBorzi <borzifrancesco@gmail.com>
2019-09-25 19:47:26 +0200

    chore: add placeholders

---
+++ src/app/services/handlers/conditions-handler.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ConditionsHandlerService } from './Conditions-handler.service';
+
+describe('ConditionsHandlerService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+    ]
+  }));
+
+  it('should be created', () => {
+    const service: ConditionsHandlerService = TestBed.get(ConditionsHandlerService);
+    expect(service).toBeTruthy();
+  });
+});
