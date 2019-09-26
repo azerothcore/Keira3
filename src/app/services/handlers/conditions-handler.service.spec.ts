7a0257d045101e56a33b2be7b31d357208bfaeee
Helias <stefanoborzi32@gmail.com>
2019-09-26 12:37:21 +0200

    Implemented unit tests for helpers and ConditionHandleService

--- src/app/services/handlers/conditions-handler.service.spec.ts
+++ src/app/services/handlers/conditions-handler.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ConditionsHandlerService } from './conditions-handler.service';
+import { Conditions } from '../../types/conditions.type';
 
 describe('ConditionsHandlerService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -10,8 +11,41 @@ describe('ConditionsHandlerService', () => {
     ]
   }));
 
-  it('should be created', () => {
+  it('should run getIdObject() correctly', () => {
     const service: ConditionsHandlerService = TestBed.get(ConditionsHandlerService);
     expect(service).toBeTruthy();
+
+    const input: Partial<Conditions> = {
+      'SourceTypeOrReferenceId': 1,
+      'SourceGroup': 2,
+      'SourceEntry': 3,
+      'SourceId': 4,
+      'ElseGroup': 4,
+      'ConditionTypeOrReference': 5,
+      'ConditionTarget': 6,
+      'ConditionValue1': 7,
+      'ConditionValue2': 8,
+      'ConditionValue3': 9,
+      'test': 10
+    };
+    const output: Partial<Conditions> = {
+      'SourceTypeOrReferenceId': 1,
+      'SourceGroup': 2,
+      'SourceEntry': 3,
+      'SourceId': 4,
+      'ElseGroup': 4,
+      'ConditionTypeOrReference': 5,
+      'ConditionTarget': 6,
+      'ConditionValue1': 7,
+      'ConditionValue2': 8,
+      'ConditionValue3': 9,
+    };
+
+    const res = service['getIdObject'](input);
+    expect(res).toEqual(output);
+
+    service.select(true, input);
+    expect(service.selected).toBe(JSON.stringify(res));
   });
+
 });
