f8cebdccb66ac2fb72f0fbdb71bb65d7b96ef3ae
FrancescoBorzi <borzifrancesco@gmail.com>
2019-10-19 15:19:58 +0200

    chore(SAI): add skeleton components

---
+++ src/app/components/editors/smart-scripts/sai-search-existing/sai-search-existing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sai-search-existing',
+  templateUrl: './sai-search-existing.component.html',
+  styleUrls: ['./sai-search-existing.component.scss']
+})
+export class SaiSearchExistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
