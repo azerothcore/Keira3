785081bf7c00f681852d2e3068e6836cc39a32a7
FrancescoBorzi <borzifrancesco@gmail.com>
2019-10-30 19:37:13 +0100

    chore(SAI): search existing sai

--- src/app/components/editors/smart-scripts/sai-search-entity/sai-search-entity.module.ts
+++ src/app/components/editors/smart-scripts/sai-search-entity/sai-search-entity.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { HighlightModule } from 'ngx-highlightjs';
 
 import { SaiSearchEntityComponent } from './sai-search-entity.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TopBarModule } from '../../shared/top-bar/top-bar.module';
+import { QueryOutputModule } from '../../shared/query-output/query-output.module';
+import { highlightOptions } from '../../../../config/highlight.config';
 
 @NgModule({
   declarations: [SaiSearchEntityComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    TopBarModule,
+    QueryOutputModule,
+    HighlightModule.forRoot(highlightOptions),
+    NgxDatatableModule,
   ],
   exports: [SaiSearchEntityComponent],
 })
--- src/app/components/editors/smart-scripts/sai-search-existing/sai-search-existing.component.ts
+++ src/app/components/editors/smart-scripts/sai-search-existing/sai-search-existing.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { SelectComplexKeyComponent } from '../../shared/select-complex-key.component';
+import { SAI_SEARCH_TYPES, SAI_SEARCH_TYPES_KEYS, SmartScripts } from '../../../../types/smart-scripts.type';
+import { SaiHandlerService } from '../../../../services/handlers/sai-handler.service';
+import { SaiSearchService } from '../../../../services/search/sai-search.service';
 
 @Component({
   selector: 'app-sai-search-existing',
   templateUrl: './sai-search-existing.component.html',
   styleUrls: ['./sai-search-existing.component.scss']
 })
-export class SaiSearchExistingComponent implements OnInit {
+export class SaiSearchExistingComponent extends SelectComplexKeyComponent<SmartScripts> {
 
-  constructor() { }
+  public readonly SAI_SEARCH_TYPES = SAI_SEARCH_TYPES;
+  public readonly SAI_SEARCH_TYPES_KEYS = SAI_SEARCH_TYPES_KEYS;
 
-  ngOnInit() {
+  constructor(
+    public selectService: SaiSearchService,
+    protected handlerService: SaiHandlerService,
+  ) {
+    super(selectService, handlerService);
   }
-
 }
