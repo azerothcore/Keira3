636651c3929e87dcba10494e2110b8a6630b3e18
Francesco Borzì <borzifrancesco@gmail.com>
2019-11-09 00:27:01 +0100

    chore(SAI): SaiTopBarComponent (#211)

--- src/app/components/editors/shared/top-bar/top-bar.component.ts
+++ src/app/components/editors/shared/top-bar/top-bar.component.ts
@@ -6,9 +6,7 @@ import { TableRow } from '../../../../types/general';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent<T extends TableRow> {
-
   @Input() handler: HandlerService<T>;
 }
