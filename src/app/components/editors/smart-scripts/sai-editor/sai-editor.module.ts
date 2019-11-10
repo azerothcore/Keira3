5647ec3ed7d3b806c3397f3383b7e4521feb7883
FrancescoBorzi <borzifrancesco@gmail.com>
2019-11-10 13:56:11 +0100

    chore: wip

--- src/app/components/editors/smart-scripts/sai-editor/sai-editor.module.ts
+++ src/app/components/editors/smart-scripts/sai-editor/sai-editor.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap';
 
 import { SaiEditorComponent } from './sai-editor.component';
+import { QueryOutputModule } from '../../shared/query-output/query-output.module';
 
 @NgModule({
   declarations: [SaiEditorComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    QueryOutputModule,
+    TooltipModule.forRoot(),
   ],
   exports: [SaiEditorComponent],
 })
