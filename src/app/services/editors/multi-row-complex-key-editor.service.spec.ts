e0eb5988b9d84815286f58daa725fab0a1f0be48
Helias <stefanoborzi32@gmail.com>
2019-11-13 13:38:03 +0100

    Working on unit tests of sai-editor and multi-row-complex-key-editor services

--- src/app/services/editors/multi-row-complex-key-editor.service.spec.ts
+++ src/app/services/editors/multi-row-complex-key-editor.service.spec.ts
@@ -1,1 +1,122 @@
-// TODO
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { instance } from 'ts-mockito';
+import Spy = jasmine.Spy;
+
+import { MultiRowComplexKeyEditorService } from './multi-row-complex-key-editor.service';
+import { QueryService } from '../query.service';
+import { MockedQueryService } from '../../test-utils/mocks';
+import { MockEntity, MockMultiRowComplexKeyEditorService, MockHandlerService } from '../../test-utils/mock-services';
+import { of, throwError } from 'rxjs';
+import { MysqlResult } from '../../types/general';
+import { MysqlError } from 'mysql';
+
+describe('SAI Editor Service', () => {
+  let service: MultiRowComplexKeyEditorService<MockEntity>;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+    ],
+    providers: [
+      { provide: QueryService, useValue: instance(MockedQueryService) }
+    ],
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(MockMultiRowComplexKeyEditorService);
+  });
+
+  it('get entityIdFields() should correcty work', () => {
+    expect(service.entityIdFields).toEqual(JSON.parse(service['_entityIdField']));
+  });
+
+  it('updateDiffQuery should correctly work', () => {
+    const queryService = TestBed.get(QueryService);
+    const getDiffDeleteInsertTwoKeysQuerySpy = spyOn(queryService, 'getDiffDeleteInsertTwoKeysQuery').and.returnValue('-- Mock Query');
+
+    service['updateDiffQuery']();
+
+    expect(getDiffDeleteInsertTwoKeysQuerySpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('addToNewRow should correctly work', () => {
+
+    const newRow = [];
+    const idx = '1';
+    const mockVal = '2';
+    service['_entityIdField'] = idx;
+    service['_loadedEntityId'] = mockVal;
+
+    service['addIdToNewRow'](newRow);
+
+    expect(newRow[idx]).toBe(Number.parseInt(mockVal, 10));
+  });
+
+  it('reload should correctly work', () => {
+
+    const resetSpy: Spy = spyOn<any>(service, 'reset');
+    const reloadEntitySpy: Spy = spyOn<any>(service, 'reloadEntity');
+
+    service.reload();
+
+    expect(service['_loading']).toBe(true);
+    expect(resetSpy).toHaveBeenCalledTimes(1);
+    expect(reloadEntitySpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('selectQuery should correctly work', () => {
+    const queryService = TestBed.get(QueryService);
+    const selectAllMultipleKeysSpy = spyOn(queryService, 'selectAllMultipleKeys').and.returnValue(of({ mock: 'data' }));
+    const handlerService = TestBed.get(MockHandlerService);
+    handlerService['_selected'] = 1;
+
+    service['selectQuery']();
+
+    expect(selectAllMultipleKeysSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('reloadEntity should correctly work', () => {
+
+    const onReloadSuccessfulSpy: Spy = spyOn<any>(service, 'onReloadSuccessful');
+    const error = { code: 'mock error', errno: 1234 } as MysqlError;
+    const selectQuerySpy: Spy = spyOn<any>(service, 'selectQuery').and.returnValue(of());
+
+    service['reloadEntity']();
+
+    expect(selectQuerySpy).toHaveBeenCalled();
+    expect(service.error).not.toBeDefined();
+    expect(service.loading).toBe(false);
+
+    selectQuerySpy.and.returnValue(throwError(error));
+
+    service['reloadEntity']();
+
+    expect(service.error).toBeDefined();
+  });
+
+  it('onReloadSuccessful should correctly work', () => {
+    const res = [{
+      id: 0,
+      guid: 0,
+      name: '',
+    }];
+    const mockData: MysqlResult<MockEntity> = { results: res };
+    const handlerService = TestBed.get(MockHandlerService);
+    const updateFullQuerySpy: Spy = spyOn<any>(service, 'updateFullQuery');
+    const disableFormSpy: Spy = spyOn(service['_form'], 'disable');
+
+    handlerService['_selected'] = 1;
+
+    service['onReloadSuccessful'](mockData);
+
+    expect(service.newRows).toEqual(res);
+    expect(service['_newRows']).toEqual([...res ]);
+    expect(service['_selectedRowId']).toBe(null);
+    expect(disableFormSpy).toHaveBeenCalledTimes(1);
+    expect(service['_loadedEntityId']).toEqual(handlerService.selected);
+    expect(service['_nextRowId']).toBe(0);
+    expect(updateFullQuerySpy).toHaveBeenCalledTimes(1);
+  });
+
+});
