5d8879a5de2f4dcd59234a2654219705a2a8b0cc
Francesco Borzì <borzifrancesco@gmail.com>
2019-12-21 17:36:09 +0100

    chore(SAI): generate template queries (#292)

--- src/app/services/handlers/sai-handler.service.spec.ts
+++ src/app/services/handlers/sai-handler.service.spec.ts
@@ -4,6 +4,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { SaiHandlerService } from './sai-handler.service';
 import { QueryService } from '../query.service';
 import { of } from 'rxjs';
+import { SAI_SEARCH_TYPES } from '../../types/smart-scripts.type';
+import { Router } from '@angular/router';
 
 describe('SaiHandlerService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -35,4 +37,52 @@ describe('SaiHandlerService', () => {
     }));
   }
 
+  const entry = 100;
+
+  for (const { sourceType, id, expectedQuery } of [
+    {
+      sourceType: SAI_SEARCH_TYPES.SAI_TYPE_CREATURE,
+      id: entry,
+      expectedQuery: `UPDATE \`creature_template\` SET \`AIName\` = 'SmartAI' WHERE \`entry\` = ${entry};`,
+    },
+    {
+      sourceType: SAI_SEARCH_TYPES.SAI_TYPE_GAMEOBJECT,
+      id: entry,
+      expectedQuery: `UPDATE \`gameobject_template\` SET \`AIName\` = 'SmartGameObjectAI' WHERE \`entry\` = ${entry};`,
+    },
+    {
+      sourceType: SAI_SEARCH_TYPES.SAI_TYPE_AREATRIGGER,
+      id: entry,
+      expectedQuery: `DELETE FROM \`areatrigger_scripts\` WHERE \`entry\` = ${entry};\n` +
+        `INSERT INTO \`areatrigger_scripts\` (\`entry\`, \`ScriptName\`) VALUES (${entry}, 'SmartTrigger');`,
+    },
+    {
+      sourceType: SAI_SEARCH_TYPES.SAI_TYPE_TIMED_ACTIONLIST,
+      id: entry,
+      expectedQuery: null,
+    },
+    {
+      sourceType: SAI_SEARCH_TYPES.SAI_TYPE_CREATURE,
+      id: -entry,
+      expectedQuery: null,
+    },
+  ]) {
+    it(`when selecting, the templateQuery should be updated [${sourceType}]`, () => {
+      const service: SaiHandlerService = TestBed.get(SaiHandlerService);
+      spyOn(TestBed.get(Router), 'navigate');
+
+      service.select(false, { source_type: sourceType, entryorguid: id });
+
+      expect(service.templateQuery).toEqual(expectedQuery);
+    });
+  }
+
+  it('navigation should not be triggered when navigate is false', () => {
+    const service: SaiHandlerService = TestBed.get(SaiHandlerService);
+    const spy = spyOn(TestBed.get(Router), 'navigate');
+
+    service.select(false, { source_type: 1, entryorguid: 123 }, 'Mock Name', false);
+
+    expect(spy).toHaveBeenCalledTimes(0);
+  });
 });
