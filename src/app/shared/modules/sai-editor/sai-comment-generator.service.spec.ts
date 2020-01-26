496af6b8c68360aefde08d4d95e55bcf55764be7
FrancescoBorzi <borzifrancesco@gmail.com>
2020-01-26 11:41:44 +0100

    chore: wip

--- src/app/shared/modules/sai-editor/sai-comment-generator.service.spec.ts
+++ src/app/shared/modules/sai-editor/sai-comment-generator.service.spec.ts
@@ -3,14 +3,32 @@ import { TestBed } from '@angular/core/testing';
 import { SaiCommentGeneratorService } from './sai-comment-generator.service';
 import { SAI_TYPES, SmartScripts } from '@keira-types/smart-scripts.type';
 import { SAI_EVENTS } from '@keira-shared/modules/sai-editor/constants/sai-event';
+import { QueryService } from '@keira-shared/services/query.service';
+import { of } from 'rxjs';
 
 fdescribe('SaiCommentGeneratorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   describe('Comment generation should correctly work', () => {
     const createSai = (partial: Partial<SmartScripts>) => Object.assign(new SmartScripts(), partial);
-
     const mockName = 'MockEntity';
+    const mockCreatureNameById = 'mockCreatureNameById';
+    const mockCreatureNameByGuid = 'mockCreatureNameByGuid';
+    const mockGameobjectNameById = 'mockGameobjectNameById';
+    const mockGameobjectNameByGuid = 'mockGameobjectNameByGuid';
+    const mockQuestTitleById = 'mockQuestTitleById';
+    const mockItemNameById = 'mockItemNameById';
+
+    beforeEach(() => {
+      const queryService = TestBed.get(QueryService);
+      spyOn(queryService, 'getCreatureNameById').and.returnValue(of(mockCreatureNameById).toPromise());
+      spyOn(queryService, 'getCreatureNameByGuid').and.returnValue(of(mockCreatureNameByGuid).toPromise());
+      spyOn(queryService, 'getGameObjectNameById').and.returnValue(of(mockGameobjectNameById).toPromise());
+      spyOn(queryService, 'getGameObjectNameByGuid').and.returnValue(of(mockGameobjectNameByGuid).toPromise());
+      spyOn(queryService, 'getQuestTitleById').and.returnValue(of(mockQuestTitleById).toPromise());
+      spyOn(queryService, 'getItemNameById').and.returnValue(of(mockItemNameById).toPromise());
+    });
+
     const cases: { name: string, input: Partial<SmartScripts>, expected: string }[] = [
       {
         name: 'Empty Creature',
@@ -48,6 +66,13 @@ fdescribe('SaiCommentGeneratorService', () => {
           source_type: SAI_TYPES.SAI_TYPE_TIMED_ACTIONLIST,
         },
         expected: ' - No Action Type',
+      },
+      {
+        name: 'Event SAI_EVENTS.ACCEPTED_QUEST',
+        input: {
+          event_type: SAI_EVENTS.ACCEPTED_QUEST,
+        },
+        expected: `MockEntity - On Quest 'mockQuestTitleById' Taken - No Action Type`,
       },
     ];
 
