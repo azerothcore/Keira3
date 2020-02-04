e7cb9731878bc9092f47e2d9e543936cc891d043
FrancescoBorzi <borzifrancesco@gmail.com>
2020-02-04 19:16:31 +0100

    feat(mysql): reconnection - Closes #343

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MysqlService } from '../shared/services/mysql.service';
+import { ToastrService } from 'ngx-toastr';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(public mysqlService: MysqlService) {}
+  constructor(
+    public mysqlService: MysqlService,
+    public toastrService: ToastrService,
+  ) {}
+
+  ngOnInit(): void {
+    this.mysqlService.connectionLost$
+      .pipe(distinctUntilChanged())
+      .subscribe((status) => {
+        if (!status) {
+          this.toastrService.error('Database connection lost');
+        } else {
+          this.toastrService.success('Database reconnected');
+        }
+      });
+  }
 }
