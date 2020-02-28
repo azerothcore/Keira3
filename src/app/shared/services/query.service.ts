392059aa54e2dbe3ff7f52670eff7ae2fb7b0e11
Francesco Borzì <borzifrancesco@gmail.com>
2020-02-28 11:34:35 +0100

    chore(sqlite): implement getDisplayIdIcon() method (#407)

--- src/app/shared/services/query.service.ts
+++ src/app/shared/services/query.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { Squel, Delete, Insert, Update } from 'squel';
 import { escape } from 'sqlstring';
--- src/app/shared/services/sqlite-query.service.spec.ts
+++ src/app/shared/services/sqlite-query.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { SqliteQueryService } from './sqlite-query.service';
 
@@ -10,7 +11,41 @@ describe('SqliteQueryService', () => {
     service = TestBed.inject(SqliteQueryService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('queryValue()', () => {
+    it('should correctly work', async () => {
+      const value = 'mock result value';
+      spyOn(service, 'query').and.returnValue(of({ v: value }));
+      const query = 'SELECT something AS v FROM my_table WHERE index = 123';
+
+      expect(await service.queryValue(query).toPromise()).toEqual(value);
+      expect(service.query).toHaveBeenCalledTimes(1);
+      expect(service.query).toHaveBeenCalledWith(query);
+    });
+
+    it('should be safe in case of no results', async () => {
+      spyOn(service, 'query').and.returnValue(of(null ));
+      const query = 'SELECT something AS v FROM my_table WHERE index = 123';
+
+      expect(await service.queryValue(query).toPromise()).toEqual(null);
+      expect(service.query).toHaveBeenCalledTimes(1);
+      expect(service.query).toHaveBeenCalledWith(query);
+    });
   });
+
+  describe('get helpers', () => {
+    const result = of('mock result');
+    const id = '123';
+
+    beforeEach(() => {
+      spyOn(service, 'queryValue').and.returnValue(result);
+    });
+
+    it('getDisplayIdIcon', () => {
+      expect(service.getDisplayIdIcon(id)).toEqual(result);
+      expect(service.queryValue).toHaveBeenCalledWith(
+        `SELECT icon AS v FROM display_icons WHERE displayId = ${id}`
+      );
+    });
+  });
+
 });
