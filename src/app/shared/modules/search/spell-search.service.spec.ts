fe04ea1db6b713cef5e6629f7d361ce6699ce7e6
Francesco Borzì <borzifrancesco@gmail.com>
2020-03-04 00:06:02 +0100

    chore(spell): implement spell search (#427)

---
+++ src/app/shared/modules/search/spell-search.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { instance } from 'ts-mockito';
+
+import { SpellSearchService } from './spell-search.service';
+import { MockedSqliteQueryService } from '../../testing/mocks';
+import { SqliteQueryService } from '@keira-shared/services/sqlite-query.service';
+
+describe('SpellSearchService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: SqliteQueryService, useValue: instance(MockedSqliteQueryService) },
+    ]
+  }));
+
+  it('should be created', () => {
+    const service: SpellSearchService = TestBed.inject(SpellSearchService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/shared/modules/search/spell-search.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { SearchService } from './search.service';
+import { SqliteQueryService } from '@keira-shared/services/sqlite-query.service';
+import {
+  SPELL_SEARCH_FIELDS,
+  SPELL_TABLE,
+  Spell,
+} from '../../types/spell.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpellSearchService extends SearchService<Spell> {
+
+  /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
+  constructor(
+    protected queryService: SqliteQueryService,
+  ) {
+    super(queryService, SPELL_TABLE, SPELL_SEARCH_FIELDS);
+  }
+}
