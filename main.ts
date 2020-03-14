f52db90f2b559a0d564b94796e4a97f23b44acc6
FrancescoBorzi <borzifrancesco@gmail.com>
2020-03-14 11:57:09 +0100

    test: fix e2e

--- main.ts
+++ main.ts
@@ -40,15 +40,17 @@ function createWindow() {
   }
 
   win.on('close', function(e) {
-    const choice = require('electron').dialog.showMessageBoxSync(this,
-      {
-        type: 'question',
-        buttons: ['Yes', 'No'],
-        title: 'Confirm',
-        message: 'Are you sure you want to quit?'
-      });
-    if (choice === 1) {
-      e.preventDefault();
+    if (!process.env.RUNNING_IN_SPECTRON) {
+      const choice = require('electron').dialog.showMessageBoxSync(this,
+        {
+          type: 'question',
+          buttons: ['Yes', 'No'],
+          title: 'Confirm',
+          message: 'Are you sure you want to quit?'
+        });
+      if (choice === 1) {
+        e.preventDefault();
+      }
     }
   });
 
