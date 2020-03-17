2c12c0a6b4d0e668fd771b52cacf6e1beaba8431
FrancescoBorzi <borzifrancesco@gmail.com>
2020-03-18 00:59:45 +0100

    chore: wip

--- src/app/features/sql-editor/sql-editor.component.ts
+++ src/app/features/sql-editor/sql-editor.component.ts
@@ -1,17 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+import { MysqlQueryService } from '@keira-shared/services/mysql-query.service';
+import { SqliteQueryService } from '@keira-shared/services/sqlite-query.service';
+import { SubscriptionHandler } from '@keira-shared/utils/subscription-handler/subscription-handler';
+import { TableRow } from '@keira-types/general';
+import { MysqlError } from 'mysql';
+import { DTCFG } from '@keira-config/datatable.config';
 
 @Component({
   selector: 'keira-sql-editor',
   templateUrl: './sql-editor.component.html',
   styleUrls: ['./sql-editor.component.scss']
 })
-export class SqlEditorComponent implements OnInit {
+export class SqlEditorComponent extends SubscriptionHandler {
+  public readonly DTCFG = DTCFG;
 
-  code = 'SELECT * FROM `creature_template` ORDER BY `entry` ASC LIMIT 100';
-
-  constructor() { }
-
-  ngOnInit(): void {
+  displayLimit = 10;
+  get displayLimitOptions() {
+    return [10, 20, 50, 100, 200, 500, 1000];
   }
 
+  code = 'SELECT `entry`, `name`, `subname`, `minlevel`, `maxlevel`, `AIName`, `ScriptName` \n' +
+    'FROM `creature_template` \n' +
+    'WHERE `entry` > 100 \n' +
+    'ORDER BY `entry` ASC LIMIT 100';
+
+  private _error: MysqlError;
+  get error(): MysqlError {
+    return this._error;
+  }
+
+  private _rows: TableRow[] = [];
+  get rows(): TableRow[] {
+    return this._rows;
+  }
+
+  private _columns;
+  get columns() {
+    return this._columns;
+  }
+
+  constructor(
+    private mysqlQueryService: MysqlQueryService,
+    private sqliteQueryService: SqliteQueryService,
+    private clipboardService: ClipboardService,
+  ) {
+    super();
+  }
+
+  copy() {
+    this.clipboardService.copyFromContent(this.code);
+  }
+
+  execute() {
+    this.subscriptions.push(this.mysqlQueryService.query(this.code).subscribe(rows => {
+      if (rows?.length > 0) {
+        this._columns = Object.keys(rows[0]);
+      }
+
+      this._rows = rows;
+    }, (error: MysqlError) => {
+      this._error = error;
+    }));
+  }
 }
--- src/app/features/sql-editor/sql-editor.module.ts
+++ src/app/features/sql-editor/sql-editor.module.ts
@@ -4,6 +4,8 @@ import { TooltipModule } from 'ngx-bootstrap';
 
 import { SqlEditorComponent } from './sql-editor.component';
 import { FormsModule } from '@angular/forms';
+import { QueryOutputModule } from '@keira-shared/modules/query-output/query-output.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [SqlEditorComponent],
@@ -11,6 +13,8 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     TooltipModule.forRoot(),
     FormsModule,
+    QueryOutputModule,
+    NgxDatatableModule,
   ]
 })
 export class SqlEditorModule { }
