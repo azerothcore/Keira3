ded94cd4053bd80ae13a9174400ae22e3cacd2ae
Francesco Borzì <borzifrancesco@gmail.com>
2020-03-21 02:41:08 +0100

    feat(dashboard): check for correct DB version (#492)

--- src/app/features/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/dashboard.component.spec.ts
@@ -8,12 +8,14 @@ import { MockedMysqlQueryService } from '@keira-testing/mocks';
 import { VersionDbRow, VersionRow } from '@keira-types/general';
 import { PageObject } from '@keira-testing/page-object';
 import { DashboardModule } from './dashboard.module';
+import { MysqlService } from '@keira-shared/services/mysql.service';
 
 class DashboardComponentPage extends PageObject<DashboardComponent> {
   get coreVersion() { return this.query<HTMLTableCellElement>('#core-version'); }
   get coreRevision() { return this.query<HTMLTableCellElement>('#core-revision'); }
   get dbVersion() { return this.query<HTMLTableCellElement>('#db-version'); }
   get dbWorldVersion() { return this.query<HTMLTableCellElement>('#db-world-version'); }
+  get dbWarning() { return this.query<HTMLDivElement>('#database-warning', false); }
 }
 
 describe('DashboardComponent', () => {
@@ -47,27 +49,34 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     when(MockedMysqlQueryService.query('SELECT * FROM version')).thenReturn(of([versionRow]));
     when(MockedMysqlQueryService.query('SELECT * FROM version_db_world')).thenReturn(of([versionDbRow]));
+    const mysqlService = TestBed.inject(MysqlService);
+    mysqlService['_config'] = { database: 'my_db' };
 
     fixture = TestBed.createComponent(DashboardComponent);
     page = new DashboardComponentPage(fixture);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should correctly display the ', () => {
+  it('should correctly display the versions', () => {
+    fixture.detectChanges();
+
     expect(page.coreVersion.innerHTML).toContain(versionRow.core_version);
     expect(page.coreRevision.innerHTML).toContain(versionRow.core_revision);
     expect(page.dbVersion.innerHTML).toContain(versionRow.db_version);
     expect(page.dbWorldVersion.innerHTML).toContain(worldDbVersion);
+    expect(page.dbWarning).toBe(null);
+    expect(component.error).toBe(false);
   });
 
   it('should correctly give error if the query does not return the data in the expected format', () => {
     when(MockedMysqlQueryService.query(anyString())).thenReturn(of([]));
     const errorSpy = spyOn(console, 'error');
 
-    component.ngOnInit();
+    fixture.detectChanges();
 
     expect(errorSpy).toHaveBeenCalledTimes(2);
+    expect(page.dbWarning).toBe(null);
+    expect(component.error).toBe(false);
   });
 
   it('should correctly give error if the query returns an error', () => {
@@ -75,9 +84,26 @@ describe('DashboardComponent', () => {
     when(MockedMysqlQueryService.query(anyString())).thenReturn(throwError(error));
     const errorSpy = spyOn(console, 'error');
 
-    component.ngOnInit();
+    fixture.detectChanges();
 
     expect(errorSpy).toHaveBeenCalledTimes(2);
     expect(errorSpy).toHaveBeenCalledWith(error);
+    expect(page.dbWarning).toBeDefined();
+    expect(component.error).toBe(true);
+  });
+
+  it('should correctly give error if the query returns an error', () => {
+    const wrongVersionRow: VersionRow = {
+      core_version: 'ShinCore rev. 2bcedc2859e7 2019-02-17 10:04:09 +0100 (master branch) (Unix, Debug)',
+      core_revision: '2bcedc2859e7',
+      db_version: 'SHINDB 335.3 (dev)',
+      cache_id: 3,
+    };
+    when(MockedMysqlQueryService.query(anyString())).thenReturn(of([wrongVersionRow]));
+
+    fixture.detectChanges();
+
+    expect(page.dbWarning).toBeDefined();
+    expect(component.error).toBe(true);
   });
 });
