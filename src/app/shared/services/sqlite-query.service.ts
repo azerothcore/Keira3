cde16e8110350c989381e89d7c192a01ed16cc3a
FrancescoBorzi <borzifrancesco@gmail.com>
2020-03-26 22:06:21 +0100

    refactor: cache

--- src/app/shared/services/sqlite-query.service.ts
+++ src/app/shared/services/sqlite-query.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { shareReplay, tap } from 'rxjs/operators';
 
 import { SqliteService } from '@keira-shared/services/sqlite.service';
 import { ConfigService } from '@keira-shared/services/config.service';
 import { TableRow } from '@keira-types/general';
 import { QueryService } from '@keira-shared/services/query.service';
+import { Lock } from '@keira-types/lock.type';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,6 @@ import { QueryService } from '@keira-shared/services/query.service';
 export class SqliteQueryService extends QueryService {
 
   private itemDisplayIdCache: Observable<string>[] = [];
-  private spellNameCache: Promise<string>[] = [];
 
   constructor(
     private sqliteService: SqliteService,
@@ -34,13 +34,6 @@ export class SqliteQueryService extends QueryService {
     );
   }
 
-  // Input query format must be: SELECT something AS v FROM ...
-  queryValue<T extends string | number>(query: string): Observable<T | null> {
-    return this.query(query).pipe(
-      map((data) => data && data[0] ? data[0].v as T : null),
-    );
-  }
-
   getIconByItemDisplayId(displayId: string | number): Observable<string> {
     displayId = Number(displayId);
 
@@ -54,14 +47,38 @@ export class SqliteQueryService extends QueryService {
   }
 
   getSpellNameById(spellId: string | number): Promise<string> {
-    spellId = Number(spellId);
+    return this.queryValueToPromiseCached('getSpellNameById', String(spellId), `SELECT spellName AS v FROM spells WHERE id = ${spellId}`);
+  }
 
-    if (!this.spellNameCache[spellId]) {
-      this.spellNameCache[spellId] = this.queryValue<string>(
-        `SELECT spellName AS v FROM spells WHERE id = ${spellId}`
-      ).pipe(shareReplay()).toPromise();
-    }
+  getSkillNameById(skillId: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT name AS v FROM skills WHERE id = ${skillId}`).toPromise();
+  }
 
-    return this.spellNameCache[spellId];
+  getFactionNameById(id: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT m_name_lang_1 AS v FROM factions WHERE m_ID = ${id}`).toPromise();
+  }
+
+  getLockById(id: string | number): Promise<Lock[]> {
+    return this.query<Lock>(`SELECT * FROM lock WHERE id = ${id}`).toPromise();
+  }
+
+  getMapNameById(id: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT m_MapName_lang1 AS v FROM maps WHERE m_ID = ${id}`).toPromise();
+  }
+
+  getAreaNameById(id: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT m_AreaName_lang AS v FROM areas_and_zones WHERE m_ID = ${id}`).toPromise();
+  }
+
+  getEventNameByHolidayId(id: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT name AS v FROM holiday WHERE id = ${id}`).toPromise();
+  }
+
+  getSocketBonusById(id: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT name AS v FROM item_enchantment WHERE id = ${id}`).toPromise();
+  }
+
+  getSpellDescriptionById(spellId: string | number): Promise<string> {
+    return this.queryValue<string>(`SELECT Description AS v FROM spells WHERE id = ${spellId}`).toPromise();
   }
 }
