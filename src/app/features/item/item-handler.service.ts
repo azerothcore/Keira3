a6bb2d9ee5a95b0a03fe5150b576d2e5de85644d
FrancescoBorzi <borzifrancesco@gmail.com>
2020-03-30 21:06:30 +0200

    fix: ench item

--- src/app/features/item/item-handler.service.ts
+++ src/app/features/item/item-handler.service.ts
@@ -8,12 +8,13 @@ import { ITEM_LOOT_TEMPLATE_TABLE } from '@keira-types/item-loot-template.type';
 import { DISENCHANT_LOOT_TEMPLATE_TABLE } from '@keira-types/disenchant-loot-template.type';
 import { PROSPECTING_LOOT_TEMPLATE_TABLE } from '@keira-types/prospecting-loot-template.type';
 import { MILLING_LOOT_TEMPLATE_TABLE } from '@keira-types/milling-loot-template.type';
+import { ITEM_ENCHANTMENT_TEMPLATE_TABLE } from '@keira-types/item-enchantment-template.type';
 
 @Injectable()
 export class ItemHandlerService extends HandlerService<ItemTemplate> {
 
   get isItemTemplateUnsaved(): boolean { return this.statusMap[ITEM_TEMPLATE_TABLE]; }
-  get isItemEnchantmentUnsaved(): boolean { return this.statusMap[ITEM_ENCHANTMENT_TABLE]; }
+  get isItemEnchantmentUnsaved(): boolean { return this.statusMap[ITEM_ENCHANTMENT_TEMPLATE_TABLE]; }
   get isItemLootTemplateUnsaved(): boolean { return this.statusMap[ITEM_LOOT_TEMPLATE_TABLE]; }
   get isDisenchantmentLootTemplateUnsaved(): boolean { return this.statusMap[DISENCHANT_LOOT_TEMPLATE_TABLE]; }
   get isProspectingLootTemplateUnsaved(): boolean { return this.statusMap[PROSPECTING_LOOT_TEMPLATE_TABLE]; }
@@ -21,7 +22,7 @@ export class ItemHandlerService extends HandlerService<ItemTemplate> {
 
   protected _statusMap = {
     [ITEM_TEMPLATE_TABLE]: false,
-    [ITEM_ENCHANTMENT_TABLE]: false,
+    [ITEM_ENCHANTMENT_TEMPLATE_TABLE]: false,
     [ITEM_LOOT_TEMPLATE_TABLE]: false,
     [DISENCHANT_LOOT_TEMPLATE_TABLE]: false,
     [PROSPECTING_LOOT_TEMPLATE_TABLE]: false,
