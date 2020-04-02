4f0ff0920166eba807c20c628846e40e8cd721a3
FrancescoBorzi <borzifrancesco@gmail.com>
2020-04-02 23:01:56 +0200

    chore: wip

--- src/app/features/quest/quest-preview/quest-preview.component.ts
+++ src/app/features/quest/quest-preview/quest-preview.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'keira-quest-preview',
   templateUrl: './quest-preview.component.html',
   styleUrls: ['./quest-preview.component.scss']
 })
-export class QuestPreviewComponent implements OnInit {
+export class QuestPreviewComponent {
+
+  showPreview = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/features/quest/quest-preview/quest-preview.module.ts
+++ src/app/features/quest/quest-preview/quest-preview.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestPreviewComponent } from './quest-preview.component';
 import { QuestPreviewService } from './quest-preview.service';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 
 
 @NgModule({
   declarations: [QuestPreviewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PerfectScrollbarModule
   ],
   providers: [QuestPreviewService],
+  exports: [
+    QuestPreviewComponent
+  ]
 })
 export class QuestPreviewModule { }
