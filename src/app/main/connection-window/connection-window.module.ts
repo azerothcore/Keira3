8b60ef41fa5103ec6bacb5205e145d9377899fa3
FrancescoBorzi <borzifrancesco@gmail.com>
2020-04-05 21:33:19 +0200

    feat(login): save connection details

--- src/app/main/connection-window/connection-window.module.ts
+++ src/app/main/connection-window/connection-window.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ConnectionWindowComponent } from './connection-window.component';
 import { QueryOutputModule } from '../../shared/modules/query-output/query-output.module';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { QueryOutputModule } from '../../shared/modules/query-output/query-outpu
     BrowserModule,
     ReactiveFormsModule,
     QueryOutputModule,
+    BsDropdownModule.forRoot(),
   ],
   exports: [
     ConnectionWindowComponent,
