088133ee272deafe0639336b6e1b1ee9c3846a64
Francesco Borzì <borzifrancesco@gmail.com>
2020-04-08 23:52:29 +0200

    feat: display message when using an older release of Keira3 (#591)

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { distinctUntilChanged } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 import { MysqlService } from '../shared/services/mysql.service';
 import { SqliteQueryService } from '@keira-shared/services/sqlite-query.service';
 import { ElectronService } from '@keira-shared/services/electron.service';
+import { KEIRA3_REPO_URL, LATEST_RELEASE_API_URL } from '@keira-constants/general';
+import { version } from '../../../package.json';
+import { SubscriptionHandler } from '@keira-shared/utils/subscription-handler/subscription-handler';
 
 @Component({
   selector: 'keira-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent extends SubscriptionHandler implements OnInit {
+  public readonly KEIRA3_REPO_URL = KEIRA3_REPO_URL;
+  showNewerVersionAlert = false;
   sqliteResult: { id: number; name: string };
 
   constructor(
@@ -20,26 +25,52 @@ export class AppComponent implements OnInit {
     public toastrService: ToastrService,
     private sqliteQueryService: SqliteQueryService,
     private electronService: ElectronService,
-  ) {}
+    private http: HttpClient,
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.mysqlService.connectionLost$
-      .pipe(distinctUntilChanged())
-      .subscribe((status) => {
-        if (!status) {
-          this.toastrService.error('Database connection lost');
-        } else {
-          this.toastrService.success('Database reconnected');
-        }
-      });
+    this.handleConnectionLostAlerts();
+    this.handleSqliteTest();
+    this.handleNewerVersionAlert();
+  }
 
+  private handleSqliteTest() {
     /* istanbul ignore next */
     if (this.electronService.isElectron()) {
-      this.sqliteQueryService.query<{ id: number, name: string}>(
-        'SELECT * FROM achievements WHERE id = 970', true
-      ).subscribe((result) => {
-        this.sqliteResult = result ? result[0] : null;
-      });
+      this.subscriptions.push(
+        this.sqliteQueryService.query<{ id: number, name: string }>(
+          'SELECT * FROM achievements WHERE id = 970', true
+        ).subscribe((result) => {
+          this.sqliteResult = result ? result[0] : null;
+        }),
+      );
     }
+  }
+
+  private handleConnectionLostAlerts() {
+    this.subscriptions.push(
+      this.mysqlService.connectionLost$
+        .pipe(distinctUntilChanged())
+        .subscribe((status) => {
+          if (!status) {
+            this.toastrService.error('Database connection lost');
+          } else {
+            this.toastrService.success('Database reconnected');
+          }
+        }),
+    );
+  }
+
+  private handleNewerVersionAlert() {
+    this.subscriptions.push(
+      this.http.get<{ tag_name: string }>(LATEST_RELEASE_API_URL).subscribe(release => {
+        const currentTag = `v${version}`;
+        if (currentTag !== release.tag_name) {
+          this.showNewerVersionAlert = true;
+        }
+      }),
+    );
   }
 }
