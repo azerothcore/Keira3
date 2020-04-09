380283a230915f8e0bb26b2266e327f2283c8e0c
Helias <stefanoborzi32@gmail.com>
2020-04-09 02:20:12 +0200

    feat(item-preview): fixed integration tests

--- src/app/features/quest/quest-preview/quest-preview.service.spec.ts
+++ src/app/features/quest/quest-preview/quest-preview.service.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuestPreviewService } from './quest-preview.service';
+import { QuestModule } from '../quest.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('QuestPreviewService', () => {
   let service: QuestPreviewService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        QuestPreviewService,
+      imports: [
+        RouterTestingModule,
+        QuestModule,
       ]
     });
     service = TestBed.inject(QuestPreviewService);
