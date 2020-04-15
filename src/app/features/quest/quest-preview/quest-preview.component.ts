8036ea16cec34148fa9947e4bcec7f95f4f6aa09
Helias <stefanoborzi32@gmail.com>
2020-04-15 22:14:43 +0200

    chore(quest-preview-classes): move logic to component

--- src/app/features/quest/quest-preview/quest-preview.component.ts
+++ src/app/features/quest/quest-preview/quest-preview.component.ts
@@ -10,6 +10,8 @@ export class QuestPreviewComponent implements OnInit {
   constructor(public readonly service: QuestPreviewService) { }
 
   get showMaxLevel(): boolean { return !!this.service.maxlevel && this.service.maxlevel !== '0'; }
+  get showRaces(): boolean { return !this.service.side && this.service.races && this.service.races.length > 0; }
+  get showClasses(): boolean { return !!this.service.classes && this.service.classes.length > 0; }
 
   ngOnInit() {
     this.service.initializeServices();
