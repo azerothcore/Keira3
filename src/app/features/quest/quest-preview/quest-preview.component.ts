1d660905688e0b9698a85565a312241d1c649e57
Stefano Borzì <stefanoborzi32@gmail.com>
2020-04-18 13:44:26 +0200

    chore(quest-preview-type): add type preview (#619)
    * chore(quest-preview-type): add type preview
    
    * chore(quest-preview-type): improve tests
    
    * chore(quest-preview): init page object
    
    * chore(quest-preview): add temporary istanbul ignore
    
    * chore(quest-preview): improve preview
    
    * chore(quest-preview): wip
    
    * chore: wip
    
    Co-authored-by: FrancescoBorzi <borzifrancesco@gmail.com>

--- src/app/features/quest/quest-preview/quest-preview.component.ts
+++ src/app/features/quest/quest-preview/quest-preview.component.ts
@@ -12,6 +12,25 @@ export class QuestPreviewComponent implements OnInit {
   get showMaxLevel(): boolean { return !!this.service.maxlevel && this.service.maxlevel !== '0'; }
   get showRaces(): boolean { return !this.service.side && this.service.races && this.service.races.length > 0; }
   get showClasses(): boolean { return !!this.service.classes && this.service.classes.length > 0; }
+  get type(): boolean { return !!this.questInfo || !!this.service.periodicQuest; }
+  get questInfo(): string {
+    return this.service.questTemplate.QuestInfoID > 0 && !!this.service.QUEST_INFO[this.service.questTemplate.QuestInfoID]
+      ? this.service.QUEST_INFO[this.service.questTemplate.QuestInfoID]
+      : null;
+  }
+
+  get questStartIcon(): string {
+    return this.service.periodicQuest
+    /* istanbul ignore next */ // TODO: test this properly using page object
+    ? 'quest_start_daily.gif'
+    : 'quest_start.gif';
+  }
+  get questEndIcon(): string {
+    return this.service.periodicQuest
+    /* istanbul ignore next */ // TODO: test this properly using page object
+    ? 'quest_end_daily.gif'
+    : 'quest_end.gif';
+  }
 
   ngOnInit() {
     this.service.initializeServices();
--- src/app/features/quest/quest-preview/quest-preview.service.spec.ts
+++ src/app/features/quest/quest-preview/quest-preview.service.spec.ts
@@ -14,6 +14,7 @@ import { QuestHandlerService } from '../quest-handler.service';
 import { MysqlQueryService } from '@keira-shared/services/mysql-query.service';
 import { of } from 'rxjs';
 import { DifficultyLevel } from './quest-preview.model';
+import { QUEST_FLAG_DAILY, QUEST_FLAG_WEEKLY, QUEST_FLAG_SPECIAL_MONTHLY } from '@keira-shared/constants/quest-preview';
 
 describe('QuestPreviewService', () => {
 
@@ -194,6 +195,21 @@ describe('QuestPreviewService', () => {
     expect(difficulty.red).toBeUndefined();
     expect(difficulty.orange).toBeUndefined();
     expect(difficulty.yellow).toBe(49);
+  });
+
+  it('periodicQuest', () => {
+    const { service, questTemplateService, questTemplateAddonService } = setup();
+
+    expect(service.periodicQuest).toBeNull();
+
+    questTemplateAddonService.form.controls.SpecialFlags.setValue(QUEST_FLAG_SPECIAL_MONTHLY);
+    expect(service.periodicQuest).toBe('Monthly');
+
+    questTemplateService.form.controls.Flags.setValue(QUEST_FLAG_WEEKLY);
+    expect(service.periodicQuest).toBe('Weekly');
+
+    questTemplateService.form.controls.Flags.setValue(QUEST_FLAG_DAILY);
+    expect(service.periodicQuest).toBe('Daily');
   });
 
   it('initializeService', () => {
