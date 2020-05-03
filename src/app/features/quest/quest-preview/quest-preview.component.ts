efacc3b9489d03ca04ca93d9149d2d13474f13c6
Helias <stefanoborzi32@gmail.com>
2020-05-03 21:03:00 +0200

    chore(lint): remove useless import

--- src/app/features/quest/quest-preview/quest-preview.component.ts
+++ src/app/features/quest/quest-preview/quest-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuestPreviewService } from './quest-preview.service';
 
 @Component({
