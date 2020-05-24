d2becaf769fb6af0091f034e71d925d17c3d60eb
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-05-24 16:21:48 +0200

    fix indentation

--- src/app/features/conditions/edit-conditions/conditions.component.ts
+++ src/app/features/conditions/edit-conditions/conditions.component.ts
@@ -89,39 +89,39 @@ export class ConditionsComponent extends SingleRowEditorComponent<Conditions> {
   }
 
   get showInstanceInfo(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_INSTANCE_INFO;
+    return this.conditionType === CONDITION_TYPES.CONDITION_INSTANCE_INFO;
   }
 
   get showLevel(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_LEVEL;
+    return this.conditionType === CONDITION_TYPES.CONDITION_LEVEL;
   }
 
   get showNearCreature(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_NEAR_CREATURE;
+    return this.conditionType === CONDITION_TYPES.CONDITION_NEAR_CREATURE;
   }
 
   get ShowObjectEntryGuid(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_OBJECT_ENTRY_GUID;
+    return this.conditionType === CONDITION_TYPES.CONDITION_OBJECT_ENTRY_GUID;
   }
 
   get ShowRelationTo(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_RELATION_TO;
+    return this.conditionType === CONDITION_TYPES.CONDITION_RELATION_TO;
   }
 
   get ShowDistanceTo(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_DISTANCE_TO;
+    return this.conditionType === CONDITION_TYPES.CONDITION_DISTANCE_TO;
   }
 
   get ShowHPVal(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_HP_VAL;
+    return this.conditionType === CONDITION_TYPES.CONDITION_HP_VAL;
   }
 
   get ShowHPPct(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_HP_PCT;
+    return this.conditionType === CONDITION_TYPES.CONDITION_HP_PCT;
   }
 
   get ShowStandState(): boolean {
-	  return this.conditionType === CONDITION_TYPES.CONDITION_STAND_STATE;
+    return this.conditionType === CONDITION_TYPES.CONDITION_STAND_STATE;
   }
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
