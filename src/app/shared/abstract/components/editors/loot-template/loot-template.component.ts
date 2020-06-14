4cf472af3d5ad0a059be14c04ad196b69d1941f4
FrancescoBorzi <borzifrancesco@gmail.com>
2020-06-14 19:36:05 +0200

    chore(misc): wip

--- src/app/shared/abstract/components/editors/loot-template/loot-template.component.ts
+++ src/app/shared/abstract/components/editors/loot-template/loot-template.component.ts
@@ -4,6 +4,7 @@ import { LootTemplate } from '@keira-types/loot-template.type';
 import { MultiRowEditorComponent } from '../multi-row-editor.component';
 import { LOOT_MODE } from '@keira-constants/flags/loot-mode';
 
+/* istanbul ignore next */ // TODO: for some reason the next line gives coverage issues...
 export abstract class LootTemplateComponent<T extends LootTemplate> extends MultiRowEditorComponent<T> implements OnInit {
 
   public readonly LOOT_MODE = LOOT_MODE;
