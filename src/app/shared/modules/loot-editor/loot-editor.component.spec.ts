109516f6327eb6f402cfe54ba39ebdf7aa40d9b4
FrancescoBorzi <borzifrancesco@gmail.com>
2020-06-15 20:30:09 +0200

    chore(misc): wip

--- src/app/shared/modules/loot-editor/loot-editor.component.spec.ts
+++ src/app/shared/modules/loot-editor/loot-editor.component.spec.ts
@@ -1,32 +1,62 @@
 import { async, TestBed } from '@angular/core/testing';
+import { Component, ViewChild } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LootEditorComponent } from './loot-editor.component';
 import { LootEditorModule } from '@keira-shared/modules/loot-editor/loot-editor.module';
 import { PageObject } from '@keira-testing/page-object';
+import { LootTemplate } from '@keira-types/loot-template.type';
+import { CreatureLootTemplateService } from '../../../features/creature/creature-loot-template/creature-loot-template.service';
+import { CreatureHandlerService } from '../../../features/creature/creature-handler.service';
+import { SaiCreatureHandlerService } from '../../../features/creature/sai-creature-handler.service';
 
-class LootEditorComponentPage extends PageObject<LootEditorComponent<any>> {
+class LootEditorComponentPage extends PageObject<TestHostComponent> {
   get referenceViewers() { return this.queryAll('keira-reference-viewer'); }
 }
 
-fdescribe('LootEditorComponent', () => {
+@Component({
+  template: '<keira-loot-editor [editorService]="editorService"></keira-loot-editor>'
+})
+class TestHostComponent {
+  @ViewChild(LootEditorComponent) child: LootEditorComponent<any>;
+  constructor(public editorService: CreatureLootTemplateService) {}
+}
+
+describe('LootEditorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [LootEditorModule],
+      declarations: [TestHostComponent],
+      imports: [
+        LootEditorModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        CreatureLootTemplateService,
+        CreatureHandlerService,
+        SaiCreatureHandlerService,
+      ]
     })
       .compileComponents();
   }));
 
   const setup = () => {
-    const fixture = TestBed.createComponent(LootEditorComponent);
+    const fixture = TestBed.createComponent(TestHostComponent);
     const page = new LootEditorComponentPage(fixture);
-    const component = fixture.componentInstance;
+    const editorService = TestBed.inject(CreatureLootTemplateService);
 
-    return { page, fixture, component };
+    return { page, fixture, editorService };
   };
 
-  it('should create', () => {
-    const { component } = setup();
-    expect(component).toBeTruthy();
+  it('should correctly show the references', () => {
+    const { page, fixture, editorService } = setup();
+    const mockLootRows: LootTemplate[] = [ new LootTemplate(), new LootTemplate(), new LootTemplate() ];
+    mockLootRows[0].Reference = 111;
+    mockLootRows[1].Reference = 222;
+    spyOnProperty(editorService, 'newRows').and.returnValue(mockLootRows);
+
+    fixture.detectChanges();
+
+    expect(page.referenceViewers.length).toEqual(2);
   });
 });
