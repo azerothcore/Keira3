18a65c4dc16649136f65b7f52b8b96c3253e7b62
FrancescoBorzi <borzifrancesco@gmail.com>
2020-06-15 19:14:56 +0200

    chore(misc): wip

--- src/app/shared/modules/loot-editor/loot-editor.component.spec.ts
+++ src/app/shared/modules/loot-editor/loot-editor.component.spec.ts
@@ -2,6 +2,11 @@ import { async, TestBed } from '@angular/core/testing';
 
 import { LootEditorComponent } from './loot-editor.component';
 import { LootEditorModule } from '@keira-shared/modules/loot-editor/loot-editor.module';
+import { PageObject } from '@keira-testing/page-object';
+
+class LootEditorComponentPage extends PageObject<LootEditorComponent<any>> {
+  get referenceViewers() { return this.queryAll('keira-reference-viewer'); }
+}
 
 fdescribe('LootEditorComponent', () => {
 
@@ -14,9 +19,10 @@ fdescribe('LootEditorComponent', () => {
 
   const setup = () => {
     const fixture = TestBed.createComponent(LootEditorComponent);
+    const page = new LootEditorComponentPage(fixture);
     const component = fixture.componentInstance;
 
-    return { fixture, component };
+    return { page, fixture, component };
   };
 
   it('should create', () => {
--- src/app/shared/modules/loot-editor/reference-viewer.component.spec.ts
+++ src/app/shared/modules/loot-editor/reference-viewer.component.spec.ts
@@ -2,6 +2,9 @@ import { async, TestBed } from '@angular/core/testing';
 
 import { ReferenceViewerComponent } from './reference-viewer.component';
 import { LootEditorModule } from '@keira-shared/modules/loot-editor/loot-editor.module';
+import { PageObject } from '@keira-testing/page-object';
+
+class ReferenceViewerComponentPage extends PageObject<ReferenceViewerComponent> {}
 
 fdescribe('ReferenceViewerComponent', () => {
 
@@ -14,9 +17,10 @@ fdescribe('ReferenceViewerComponent', () => {
 
   const setup = () => {
     const fixture = TestBed.createComponent(ReferenceViewerComponent);
+    const page = new ReferenceViewerComponentPage(fixture);
     const component = fixture.componentInstance;
 
-    return { fixture, component };
+    return { page, fixture, component };
   };
 
   it('should create', () => {
