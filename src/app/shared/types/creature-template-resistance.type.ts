9bece79ab7a7a5f9f4a6205368b16d27ec7d655f
Stefano Borzì <stefanoborzi32@gmail.com>
2021-01-28 20:27:10 +0100

    fix(Creature): creature template resistance type

--- src/app/shared/types/creature-template-resistance.type.ts
+++ src/app/shared/types/creature-template-resistance.type.ts
@@ -1,12 +1,12 @@
 import { TableRow } from './general';
 
 export const CREATURE_TEMPLATE_RESISTANCE_TABLE = 'creature_template_resistance';
-export const CREATURE_TEMPLATE_RESISTANCE_ID = 'creatureid';
-export const CREATURE_TEMPLATE_SCHOOL = 'school';
+export const CREATURE_TEMPLATE_RESISTANCE_ID = 'CreatureID';
+export const CREATURE_TEMPLATE_SCHOOL = 'School'
 
 export class CreatureTemplateResistance extends TableRow {
-  creatureid: number = 0;
-  school: number = 0;
-  resistance: number = 0;
+  CreatureID: number = 0;
+  School: number = 0;
+  Resistance: number = 0;
   verifiedbuild: number = 0;
 }
