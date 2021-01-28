068825b74fe802df26f6f8628c04d50fbbf3e831
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-29 00:50:32 +0100

    rename option

--- src/app/features/creature/creature-template-resistance/creature-template-resistance.component.ts
+++ src/app/features/creature/creature-template-resistance/creature-template-resistance.component.ts
@@ -4,7 +4,7 @@ import { CreatureHandlerService } from '../creature-handler.service';
 import { CreatureTemplateResistance } from '@keira-types/creature-template-resistance.type';
 import { CreatureTemplateResistanceService } from './creature-template-resistance.service';
 import { CREATURE_TEMPLATE_RESISTANCE_TABLE } from '@keira-types/creature-template-resistance.type';
-import { SPELLSCHOOLRES } from '@keira-constants/options/spell-school-res';
+import { RESISTANCE } from '@keira-constants/options/creature-template-resistance';
 import { MultiRowEditorComponent } from '@keira-shared/abstract/components/editors/multi-row-editor.component';
 
 @Component({
@@ -18,7 +18,7 @@ export class CreatureTemplateResistanceComponent extends MultiRowEditorComponent
     return this.WIKI_BASE_URL + CREATURE_TEMPLATE_RESISTANCE_TABLE;
   }
 
-  public readonly SPELLSCHOOLRES = SPELLSCHOOLRES;
+  public readonly RESISTANCE = RESISTANCE;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
