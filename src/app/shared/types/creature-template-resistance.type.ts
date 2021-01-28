66f8e8633572f6934380ca7f75c78c3ea0027a8c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-28 20:53:06 +0100

    Build

--- src/app/shared/types/creature-template-resistance.type.ts
+++ src/app/shared/types/creature-template-resistance.type.ts
@@ -2,7 +2,7 @@ import { TableRow } from './general';
 
 export const CREATURE_TEMPLATE_RESISTANCE_TABLE = 'creature_template_resistance';
 export const CREATURE_TEMPLATE_RESISTANCE_ID = 'CreatureID';
-export const CREATURE_TEMPLATE_SCHOOL = 'School'
+export const CREATURE_TEMPLATE_SCHOOL = 'School';
 
 export class CreatureTemplateResistance extends TableRow {
   CreatureID: number = 0;
