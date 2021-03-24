b141681b2e20793a3562266662fefe5431857e3b
Helias <stefanoborzi32@gmail.com>
2021-03-24 22:22:19 +0100

    chore: improve spell index suggestions

--- src/app/features/creature/creature-template-spell/creature-template-spell.component.ts
+++ src/app/features/creature/creature-template-spell/creature-template-spell.component.ts
@@ -5,7 +5,6 @@ import { CreatureTemplateSpell } from '@keira-types/creature-template-spell.type
 import { CreatureTemplateSpellService } from './creature-template-spell.service';
 import { CREATURE_TEMPLATE_SPELL_TABLE } from '@keira-types/creature-template-spell.type';
 import { MultiRowEditorComponent } from '@keira-shared/abstract/components/editors/multi-row-editor.component';
-import { CREATURE_TEMPLATE_SPELL_INDEX } from '@keira-constants/options/creature-template-spell-index';
 
 @Component({
   selector: 'keira-creature-template-spell',
@@ -16,8 +15,6 @@ export class CreatureTemplateSpellComponent extends MultiRowEditorComponent<Crea
   public get docUrl(): string {
     return this.WIKI_BASE_URL + CREATURE_TEMPLATE_SPELL_TABLE;
   }
-
-  public readonly CREATURE_TEMPLATE_SPELL_INDEX = CREATURE_TEMPLATE_SPELL_INDEX;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(
