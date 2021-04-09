9ba36fbc3ddffee52e710e326a2db43224f6a46e
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-09 14:45:18 +0200

    chore(flags): Update to AC

--- src/app/shared/constants/flags/unit-flags2.ts
+++ src/app/shared/constants/flags/unit-flags2.ts
@@ -2,11 +2,11 @@ import { Flag } from '../../types/general';
 
 export const UNIT_FLAGS_2: Flag[] = [
   { bit: 0,  name: 'FEIGN_DEATH'},
-  { bit: 1,  name: 'UNK1 - Hide unit model (show only player equip)'},
+  { bit: 1,  name: 'HIDE_BODY - Hide unit model (show only player equip)'},
   { bit: 2,  name: 'IGNORE_REPUTATION'},
   { bit: 3,  name: 'COMPREHEND_LANG'},
   { bit: 4,  name: 'MIRROR_IMAGE'},
-  { bit: 5,  name: 'INSTANTLY_APPEAR_MODEL - Unit model instantly appears when summoned (does not fade in)'},
+  { bit: 5,  name: 'DO_NOT_FADE_IN - Unit model instantly appears when summoned (does not fade in)'},
   { bit: 6,  name: 'FORCE_MOVEMENT'},
   { bit: 7,  name: 'DISARM_OFFHAND'},
   { bit: 8,  name: 'DISABLE_PRED_STATS - Player has disabled predicted stats (Used by raid frames)'},
@@ -15,7 +15,7 @@ export const UNIT_FLAGS_2: Flag[] = [
   { bit: 12, name: 'RESTRICT_PARTY_INTERACT - Restrict interaction to party or raid'},
   { bit: 13, name: 'PREVENT_SPELL_CLICK - Prevent spellclick'},
   { bit: 14, name: 'ALLOW_ENEMY_INTERACT'},
-  { bit: 15, name: 'DISABLE_TURN'},
+  { bit: 15, name: 'CANNOT_TURN'},
   { bit: 16, name: 'UNK2'},
   { bit: 17, name: 'PLAY_DEATH_ANIM - Plays special death animation upon death'},
   { bit: 18, name: 'ALLOW_CHEAT_SPELLS - Allows casting spells with AttributesEx7 & SPELL_ATTR7_IS_CHEAT_SPELL'},
