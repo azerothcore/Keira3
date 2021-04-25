3c99440929f93c880f07e07d2206031f12ba0642
FrancescoBorzi <borzifrancesco@gmail.com>
2021-04-25 19:46:53 +0200

    chore(spell-dbc): minimal editor

--- src/app/features/spell/spell-dbc/spell-dbc.module.ts
+++ src/app/features/spell/spell-dbc/spell-dbc.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpellDbcComponent } from './spell-dbc.component';
+import { TopBarModule } from '@keira-shared/modules/top-bar/top-bar.module';
+import { QueryOutputModule } from '@keira-shared/modules/query-output/query-output.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 
 
@@ -9,7 +13,11 @@ import { SpellDbcComponent } from './spell-dbc.component';
     SpellDbcComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TopBarModule,
+    QueryOutputModule,
+    ReactiveFormsModule,
+    TooltipModule
   ]
 })
 export class SpellDbcModule { }
