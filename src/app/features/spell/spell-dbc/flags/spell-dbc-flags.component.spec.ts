83f32d21e45afe179e29d1ace8f5526b04e15363
Francesco Borzì <borzifrancesco@gmail.com>
2021-05-09 16:00:41 +0200

    chore(spell-dbc): flags fields (#1216)

--- src/app/features/spell/spell-dbc/flags/spell-dbc-flags.component.spec.ts
+++ src/app/features/spell/spell-dbc/flags/spell-dbc-flags.component.spec.ts
@@ -34,6 +34,21 @@ describe('SpellDbcFlagsComponent', () => {
       .compileComponents();
   });
 
+  const fields: string[] = [
+    'InterruptFlags',
+    'AuraInterruptFlags',
+    'ChannelInterruptFlags',
+    'Attributes',
+    'AttributesEx',
+    'AttributesEx2',
+    'AttributesEx3',
+    'AttributesEx4',
+    'AttributesEx5',
+    'AttributesEx6',
+    'AttributesEx7',
+  ];
+  const createMockVal = (field: string): number => field.length;
+
   const setup = () => {
     const fixture = TestBed.createComponent(TestHostComponent);
     const host = fixture.componentInstance;
@@ -47,8 +62,38 @@ describe('SpellDbcFlagsComponent', () => {
     return { fixture, component, page, form };
   };
 
-  it('should create', () => {
-    const { component } = setup();
-    expect(component).toBeTruthy();
+  it('should correctly display all fields', () => {
+    const { page } = setup();
+
+    for (const field of fields) {
+      page.expectInputVisible(field);
+    }
+  });
+
+  it('should correctly display the values of the form', async () => {
+    const { page, form } = setup();
+
+    for (const field of fields) {
+      form.getControl(field).setValue(createMockVal(field));
+    }
+    page.detectChanges();
+
+    for (const field of fields) {
+      expect(page.getInput(field).value).toEqual(String(createMockVal(field)));
+    }
+  });
+
+  it('should correctly edit the values of the form', () => {
+    const { page, form} = setup();
+    page.detectChanges();
+
+    for (const field of fields) {
+      page.setInputValue(page.getInput(field), createMockVal(field));
+    }
+    page.detectChanges();
+
+    for (const field of fields) {
+      expect(form.getControl(field).value).toEqual(createMockVal(field));
+    }
   });
 });
