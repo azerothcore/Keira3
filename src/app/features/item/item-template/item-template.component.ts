59d78f2b012c9a779f25563f85e729f2babf999b
FrancescoBorzi <borzifrancesco@gmail.com>
2021-05-16 17:46:19 +0200

    test

--- src/app/features/item/item-template/item-template.component.ts
+++ src/app/features/item/item-template/item-template.component.ts
@@ -54,6 +54,7 @@ export class ItemTemplateComponent extends SingleRowEditorComponent<ItemTemplate
   public readonly STAT_TYPE = STAT_TYPE;
   public readonly PVP_RANK = PVP_RANK;
 
+
   showItemPreview = true;
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
