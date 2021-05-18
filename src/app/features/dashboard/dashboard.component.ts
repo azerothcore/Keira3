f67075a26875926d2a5179811e5887bd90bfa754
Francesco Borzì <borzifrancesco@gmail.com>
2021-05-18 19:23:10 +0200

    feat(deps): Angular 12 (#1239)

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { MysqlQueryService } from '../../shared/services/mysql-query.service';
 import { VersionDbRow, VersionRow } from '@keira-types/general';
-import { version } from '../../../../package.json';
+import packageInfo from '../../../../package.json';
 import { AC_FORUM_URL, PAYPAL_DONATE_URL, KEIRA3_REPO_URL, AC_DISCORD_URL } from '@keira-constants/general';
 import { SubscriptionHandler } from '@keira-shared/utils/subscription-handler/subscription-handler';
 import { ConfigService } from '@keira-shared/services/config.service';
@@ -17,7 +17,7 @@ export class DashboardComponent extends SubscriptionHandler implements OnInit {
   coreVersions: VersionRow;
   dbWorldVersion: string;
   error = false;
-  public readonly KEIRA_VERSION = version;
+  public readonly KEIRA_VERSION = packageInfo.version;
   public readonly PAYPAL_DONATE_URL = PAYPAL_DONATE_URL;
   public readonly AC_FORUM_URL = AC_FORUM_URL;
   public readonly AC_DISCORD_URL = AC_DISCORD_URL;
@@ -27,11 +27,7 @@ export class DashboardComponent extends SubscriptionHandler implements OnInit {
     return this.mysqlService.config.database;
   }
 
-  constructor(
-    private queryService: MysqlQueryService,
-    public configService: ConfigService,
-    private readonly mysqlService: MysqlService,
-  ) {
+  constructor(private queryService: MysqlQueryService, public configService: ConfigService, private readonly mysqlService: MysqlService) {
     super();
   }
 
@@ -49,10 +45,7 @@ export class DashboardComponent extends SubscriptionHandler implements OnInit {
           if (data && data.length > 0) {
             this.coreVersions = data[0];
 
-            if (
-              !this.coreVersions.db_version.startsWith('ACDB') ||
-              !this.coreVersions.core_version.startsWith('AzerothCore')
-            ) {
+            if (!this.coreVersions.db_version.startsWith('ACDB') || !this.coreVersions.core_version.startsWith('AzerothCore')) {
               this.error = true;
             }
           } else {
