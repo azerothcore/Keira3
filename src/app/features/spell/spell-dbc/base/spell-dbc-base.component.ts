ed37ba61d2adaf918c9603e800c16eeb1ffb3bcc
Stefano Borzì <stefanoborzi32@gmail.com>
2021-05-29 11:56:16 +0200

    chore: add selectors for spell-dbc (#1270)

--- src/app/features/spell/spell-dbc/base/spell-dbc-base.component.ts
+++ src/app/features/spell/spell-dbc/base/spell-dbc-base.component.ts
@@ -3,15 +3,20 @@ import { FormGroup } from 'ngx-typesafe-forms';
 import { SpellDbc } from '@keira-types/spell-dbc.type';
 import { SPELL_DBC_SCHOOL_OPTIONS } from '@keira-shared/constants/flags/spell_dbc_school_options';
 import { SPELL_DBC_FACING_FRONT_FLAG } from '@keira-shared/constants/flags/spell_dbc_facing_front_flag';
+import { DISPEL_TYPE } from '@keira-shared/constants/options/dispel-type';
+import { SPELL_MECHANIC } from '@keira-shared/constants/options/spell-mechanic';
+import { TOTEM_CATEGORY } from '@keira-shared/constants/options/totem-category';
 
 @Component({
   selector: 'keira-spell-dbc-base',
   templateUrl: './spell-dbc-base.component.html',
 })
 export class SpellDbcBaseComponent {
-	
-  public readonly SPELL_DBC_SCHOOL_OPTIONS = SPELL_DBC_SCHOOL_OPTIONS;
-  public readonly SPELL_DBC_FACING_FRONT_FLAG = SPELL_DBC_FACING_FRONT_FLAG;
-	
+  readonly SPELL_DBC_SCHOOL_OPTIONS = SPELL_DBC_SCHOOL_OPTIONS;
+  readonly SPELL_DBC_FACING_FRONT_FLAG = SPELL_DBC_FACING_FRONT_FLAG;
+  readonly DISPEL_TYPE = DISPEL_TYPE;
+  readonly SPELL_MECHANIC = SPELL_MECHANIC;
+  readonly TOTEM_CATEGORY = TOTEM_CATEGORY;
+
   @Input() formGroup: FormGroup<SpellDbc>;
 }
