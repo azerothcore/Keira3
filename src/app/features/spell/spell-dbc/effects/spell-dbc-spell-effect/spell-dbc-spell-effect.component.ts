ffc4044615dfc8162c5a23398ce6fa4f284e2c6c
Helias <stefanoborzi32@gmail.com>
2021-06-10 20:16:02 +0200

    chore: add mechanic selectors

--- src/app/features/spell/spell-dbc/effects/spell-dbc-spell-effect/spell-dbc-spell-effect.component.ts
+++ src/app/features/spell/spell-dbc/effects/spell-dbc-spell-effect/spell-dbc-spell-effect.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FormGroup } from 'ngx-typesafe-forms';
 import { SpellDbc } from '@keira-types/spell-dbc.type';
 import { SpellDbcSpellEffectFieldPrefix } from './spell-dbc-spell-effect.model';
+import { SPELL_MECHANIC } from '@keira-shared/constants/options/spell-mechanic';
 
 @Component({
   selector: 'keira-spell-dbc-spell-effect',
@@ -10,6 +11,8 @@ import { SpellDbcSpellEffectFieldPrefix } from './spell-dbc-spell-effect.model';
 export class SpellDbcSpellEffectComponent {
   @Input() formGroup: FormGroup<SpellDbc>;
   @Input() index: number;
+
+  readonly SPELL_MECHANIC = SPELL_MECHANIC;
 
   getFieldName(field: SpellDbcSpellEffectFieldPrefix): string {
     return `${field}_${this.index}`; // example: EffectSpellClassMaskA_3
