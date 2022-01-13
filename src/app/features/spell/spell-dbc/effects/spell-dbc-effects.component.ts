93cfb2371d3a12e8de0f66b75acfb2988e44dfdd
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2022-01-13 11:28:13 +0100

    feat(spell): added Targets flag selector

--- src/app/features/spell/spell-dbc/effects/spell-dbc-effects.component.ts
+++ src/app/features/spell/spell-dbc/effects/spell-dbc-effects.component.ts
@@ -3,11 +3,14 @@ import { FormGroup } from 'ngx-typesafe-forms';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ToastrModule } from 'ngx-toastr';
 import { SpellDbc } from '@keira-types/spell-dbc.type';
+import { SPELL_DBC_TARGETS } from '@keira-shared/constants/flags/spell_dbc_targets';
 
 @Component({
   selector: 'keira-spell-dbc-effects',
   templateUrl: './spell-dbc-effects.component.html',
 })
 export class SpellDbcEffectsComponent {
+  readonly SPELL_DBC_TARGETS = SPELL_DBC_TARGETS;
+
   @Input() formGroup: FormGroup<SpellDbc>;
 }
