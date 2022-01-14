a56483c8441cbf6c4ad4e03d167e2a9185d74aa3
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2022-01-14 13:32:22 +0100

    feat(spell): added tooltips and SpellClassMask selector in effect tab

--- src/app/features/spell/spell-dbc/effects/spell-dbc-spell-effect/spell-dbc-spell-effect.component.ts
+++ src/app/features/spell/spell-dbc/effects/spell-dbc-spell-effect/spell-dbc-spell-effect.component.ts
@@ -6,6 +6,7 @@ import { SpellDbc } from '@keira-types/spell-dbc.type';
 import { SpellDbcSpellEffectFieldPrefix } from './spell-dbc-spell-effect.model';
 import { SPELL_MECHANIC } from '@keira-shared/constants/options/spell-mechanic';
 import { SPELL_DBC_EFFECT, SPELL_DBC_APPLY_AURA_NAME } from '@keira-shared/constants/options/spell_dbc_effects_options';
+import { SPELL_DBC_CLASS_MASK_FLAGS } from '@keira-shared/constants/flags/spell_dbc_effect_flags';
 
 @Component({
   selector: 'keira-spell-dbc-spell-effect',
@@ -18,6 +19,7 @@ export class SpellDbcSpellEffectComponent {
   public readonly SPELL_MECHANIC = SPELL_MECHANIC;
   public readonly SPELL_DBC_EFFECT = SPELL_DBC_EFFECT;
   public readonly SPELL_DBC_APPLY_AURA_NAME = SPELL_DBC_APPLY_AURA_NAME;
+  public readonly SPELL_DBC_CLASS_MASK_FLAGS = SPELL_DBC_CLASS_MASK_FLAGS;
 
   getFieldName(field: SpellDbcSpellEffectFieldPrefix): string {
     return `${field}_${this.index}`; // example: EffectSpellClassMaskA_3
