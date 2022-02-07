3f9a9c8f6c98094ca5a6b20da865f45cb7d84dee
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2022-02-07 12:43:26 +0100

    fix(spell): grouped and sorted item fields

--- src/app/features/spell/spell-dbc/base/spell-dbc-base.component.ts
+++ src/app/features/spell/spell-dbc/base/spell-dbc-base.component.ts
@@ -7,7 +7,6 @@ import { SPELL_SCHOOL_MASK } from '@keira-shared/constants/flags/spell-school-ma
 import { SPELL_DBC_FACING_FRONT_FLAG } from '@keira-shared/constants/flags/spell_dbc_base_flags';
 import { DISPEL_TYPE } from '@keira-shared/constants/options/dispel-type';
 import { SPELL_MECHANIC } from '@keira-shared/constants/options/spell-mechanic';
-import { TOTEM_CATEGORY } from '@keira-shared/constants/options/totem-category';
 import {
   SPELL_DBC_CAST_TIME,
   SPELL_DBC_POWER_TYPE,
@@ -25,7 +24,6 @@ export class SpellDbcBaseComponent {
   readonly SPELL_DBC_FACING_FRONT_FLAG = SPELL_DBC_FACING_FRONT_FLAG;
   readonly DISPEL_TYPE = DISPEL_TYPE;
   readonly SPELL_MECHANIC = SPELL_MECHANIC;
-  readonly TOTEM_CATEGORY = TOTEM_CATEGORY;
   readonly SPELL_DBC_POWER_TYPE = SPELL_DBC_POWER_TYPE;
   readonly SPELL_DBC_CAST_TIME = SPELL_DBC_CAST_TIME;
   readonly SPELL_DBC_CASTER_AURA_STATE = SPELL_DBC_CASTER_AURA_STATE;
--- src/app/features/spell/spell-dbc/items/spell-dbc-items.component.ts
+++ src/app/features/spell/spell-dbc/items/spell-dbc-items.component.ts
@@ -4,6 +4,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ToastrModule } from 'ngx-toastr';
 import { SpellDbc } from '@keira-types/spell-dbc.type';
 import { ITEM_CLASS } from '@keira-shared/constants/options/item-class';
+import { TOTEM_CATEGORY } from '@keira-shared/constants/options/totem-category';
 import { SPELL_DBC_ITEM_SUBCLASS, SPELL_DBC_INVENTORY_TYPE } from '@keira-shared/constants/flags/spell_dbc_item_flags';
 
 @Component({
@@ -14,6 +15,7 @@ export class SpellDbcItemsComponent {
   public readonly ITEM_CLASS = ITEM_CLASS;
   public readonly SPELL_DBC_ITEM_SUBCLASS = SPELL_DBC_ITEM_SUBCLASS;
   public readonly SPELL_DBC_INVENTORY_TYPE = SPELL_DBC_INVENTORY_TYPE;
+  public readonly TOTEM_CATEGORY = TOTEM_CATEGORY;
 
   @Input() formGroup: FormGroup<SpellDbc>;
 }
