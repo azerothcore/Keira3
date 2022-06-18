bc4eaa40b638b7d01c438cf599f55f99a54a522f
Helias <stefanoborzi32@gmail.com>
2022-06-18 18:54:31 +0200

    feat: add translations for Gossip

--- src/app/features/gossip/gossip-menu-option-preview/gossip-menu-option-preview.module.ts
+++ src/app/features/gossip/gossip-menu-option-preview/gossip-menu-option-preview.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { QueryOutputModule } from '@keira-shared/modules/query-output/query-output.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { GossipMenuOptionPreviewComponent } from './gossip-menu-option-preview.component';
 
 @NgModule({
   declarations: [GossipMenuOptionPreviewComponent],
-  imports: [BrowserModule, QueryOutputModule, PerfectScrollbarModule],
+  imports: [BrowserModule, QueryOutputModule, PerfectScrollbarModule, TranslateModule],
   exports: [GossipMenuOptionPreviewComponent],
 })
 export class GossipMenuOptionPreviewModule {}
