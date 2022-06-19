6526b7fb123280df116407c09433b11915042d01
Helias <stefanoborzi32@gmail.com>
2022-06-19 13:09:38 +0200

    feat: add several translations to category titles and tooltips

--- src/app/features/creature/creature-onkill-reputation/creature-onkill-reputation.component.ts
+++ src/app/features/creature/creature-onkill-reputation/creature-onkill-reputation.component.ts
@@ -13,11 +13,6 @@ import { CreatureOnkillReputationService } from './creature-onkill-reputation.se
 export class CreatureOnkillReputationComponent extends SingleRowEditorComponent<CreatureOnkillReputation> {
   public readonly FACTION_RANK = FACTION_RANK;
 
-  public readonly TEAM_DEPENDENT_TOOLTIP =
-    'Boolean value. 0 - Will give reputation to the any player from both fields ' +
-    '(RewOnKillRepFaction1 and RewOnKillRepFaction2); 1 - Will give reputation to alliance players from RewOnKillRepFaction1 and ' +
-    'horde players from RewOnKillRepFaction2';
-
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(public editorService: CreatureOnkillReputationService, public handlerService: CreatureHandlerService) {
     super(editorService, handlerService);
--- src/app/features/creature/creature-spawn/creature-spawn.component.ts
+++ src/app/features/creature/creature-spawn/creature-spawn.component.ts
@@ -23,10 +23,6 @@ export class CreatureSpawnComponent extends MultiRowEditorComponent<CreatureSpaw
   public readonly SPAWN_MASK = SPAWN_MASK;
   public readonly PHASE_MASK = PHASE_MASK;
 
-  public readonly SPAWN_DIST_TOOLTIP =
-    'The maximum distance that the creature may spawn from its spawn point. ' +
-    'Also controls how far away the creature can walk from its spawn point if its MovementType = 1.';
-
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
   constructor(public editorService: CreatureSpawnService, public handlerService: CreatureHandlerService) {
     super(editorService, handlerService);
