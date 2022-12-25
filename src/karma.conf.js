d300852bedc2869127d29b6e88d07a55dbc1fe91
FrancescoBorzi <borzifrancesco@gmail.com>
2022-12-25 16:27:12 +0100

    test: fix coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,15 +21,22 @@ module.exports = function (config) {
         timeoutInterval: 10000,
       },
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      subdir: '.',
       fixWebpackSourcePaths: true,
-      thresholds: {
-        statements: 100,
-        lines: 100,
-        branches: 100,
-        functions: 100,
+      reporters: [
+        { type: 'text-summary', file: 'coverage.txt' },
+        { type: 'html', file: 'coverage.txt' },
+        { type: 'lcovonly', subdir: './', file: 'coverage.txt' },
+      ],
+      check: {
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+        },
       },
     },
     reporters: ['progress', 'kjhtml'],
