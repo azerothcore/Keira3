51d1cffc02e1e132ad65f529945a59444ca74443
Stefano Borzì <stefanoborzi32@gmail.com>
2023-01-09 17:36:40 +0100

    fix: switch language persistent (#2086)
    * fix: switch language persistent
    
    * test: fix unit tests for switch-language
    
    * test: remove useless mock module

--- src/app/shared/modules/switch-language/switch-language.module.ts
+++ src/app/shared/modules/switch-language/switch-language.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
 import { SwitchLanguageComponent } from './switch-language.component';
+import { SwitchLanguageService } from './switch-language.service';
 
 @NgModule({
   declarations: [SwitchLanguageComponent],
   imports: [BrowserModule, FormsModule, TranslateModule],
+  providers: [SwitchLanguageService],
   exports: [SwitchLanguageComponent],
 })
 export class SwitchLanguageModule {}
