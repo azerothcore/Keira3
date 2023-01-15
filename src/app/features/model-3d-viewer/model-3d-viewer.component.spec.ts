57cf4c5700fb865ffffb321550d81990b27e4ddf
Stefano Borzì <stefanoborzi32@gmail.com>
2023-01-15 13:24:23 +0100

    feat: show all creature, item and gobject 3D models (#2093)
    * feat: remove perfect-scrollbar, closes #2053
    
    * chore: add missing css class
    
    * feat: add all creature 3D models in the preview
    
    * test: add model-3d-viewer component unit tests, clean a bit the code

---
+++ src/app/features/model-3d-viewer/model-3d-viewer.component.spec.ts
@@ -0,0 +1,184 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { MysqlQueryService } from '@keira-shared/services/mysql-query.service';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { of, Subscription } from 'rxjs';
+import { Model3DViewerComponent } from './model-3d-viewer.component';
+import { CONTENT_WOTLK, MODEL_TYPE, VIEWER_TYPE } from './model-3d-viewer.model';
+
+describe('Model3DViewerComponent', () => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [Model3DViewerComponent],
+      imports: [ModalModule.forRoot(), HttpClientTestingModule],
+    }).compileComponents();
+  }));
+
+  const mockDisplayId = 1;
+
+  function setup() {
+    const fixture = TestBed.createComponent(Model3DViewerComponent);
+    const component = fixture.componentInstance;
+    const queryService = TestBed.inject(MysqlQueryService);
+    const httpTestingController = TestBed.inject(HttpTestingController);
+
+    fixture.detectChanges();
+
+    return { fixture, component, queryService, httpTestingController };
+  }
+
+  it('ngOnInit', () => {
+    const { component } = setup();
+    spyOn<any>(component, 'resetModel3dElement');
+    spyOn<any>(component, 'viewerDynamic');
+
+    component.ngOnInit();
+
+    expect(component['resetModel3dElement']).toHaveBeenCalledTimes(1);
+    expect(component['viewerDynamic']).toHaveBeenCalledTimes(1);
+  });
+
+  it('ngOnChanges', () => {
+    const { component } = setup();
+    const resetModel3dElementSpy = spyOn<any>(component, 'resetModel3dElement');
+    const show3DmodelSpy = spyOn(component, 'show3Dmodel');
+    component.displayId = 2;
+    component.viewerType = VIEWER_TYPE.NPC;
+
+    component.ngOnChanges({ displayId: { currentValue: 1, previousValue: 0 } as any });
+
+    expect(resetModel3dElementSpy).toHaveBeenCalledTimes(1);
+    expect(show3DmodelSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('ngOnDestroy', () => {
+    const { component } = setup();
+    component['subscriptions'] = new Subscription();
+    const unsubscribeSpy = spyOn<any>(component['subscriptions'], 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(unsubscribeSpy).toHaveBeenCalledTimes(1);
+  });
+
+  describe('show3Dmodel', () => {
+    it('generate3Dmodel for non-item', () => {
+      const { component } = setup();
+      component.viewerType = VIEWER_TYPE.NPC;
+      spyOn<any>(component, 'generate3Dmodel');
+
+      component.show3Dmodel();
+
+      expect(component['generate3Dmodel']).toHaveBeenCalledTimes(1);
+    });
+
+    it('handles the item 3D model', (done) => {
+      const { component } = setup();
+      component.viewerType = VIEWER_TYPE.ITEM;
+      component['subscriptions'] = new Subscription();
+      const subscriptionAddSpy = spyOn<any>(component['subscriptions'], 'add');
+      const mockItemData$ = of([{ entry: 123 }]);
+      spyOn<any>(component, 'getItemData$').and.returnValue(mockItemData$);
+      spyOn<any>(component, 'verifyModelAndLoad');
+
+      component.show3Dmodel();
+
+      mockItemData$.subscribe(() => {
+        expect(subscriptionAddSpy).toHaveBeenCalledTimes(1);
+        expect(component['verifyModelAndLoad']).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
+  });
+
+  describe('getContentPathUrl', () => {
+    it('returns armor URL', () => {
+      const { component } = setup();
+      component.displayId = mockDisplayId;
+
+      expect(component['getContentPathUrl'](3)).toBe(`${CONTENT_WOTLK}meta/armor/3/${mockDisplayId}.json`);
+      expect(component['getContentPathUrl'](4)).toBe(`${CONTENT_WOTLK}meta/armor/4/${mockDisplayId}.json`);
+    });
+
+    it('returns item URL', () => {
+      const { component } = setup();
+      component.displayId = mockDisplayId;
+
+      expect(component['getContentPathUrl'](1)).toBe(`${CONTENT_WOTLK}meta/item/${mockDisplayId}.json`);
+    });
+  });
+
+  describe('getModelType', () => {
+    it('uses default params', () => {
+      const { component } = setup();
+      component.itemInventoryType = 1;
+      component.itemClass = 2;
+      component.viewerType = VIEWER_TYPE.ITEM;
+
+      expect(component['getModelType']()).toBe(MODEL_TYPE.WEAPON);
+    });
+
+    it('uses custom params', () => {
+      const { component } = setup();
+      component.itemInventoryType = 1;
+      component.itemClass = 2;
+      component.viewerType = VIEWER_TYPE.ITEM;
+
+      expect(component['getModelType'](3, 1)).toBe(MODEL_TYPE.HELMET);
+      expect(component['getModelType'](3, 3)).toBe(MODEL_TYPE.SHOULDER);
+    });
+
+    it('returns object', () => {
+      const { component } = setup();
+      component.viewerType = VIEWER_TYPE.OBJECT;
+      expect(component['getModelType']()).toBe(MODEL_TYPE.OBJECT);
+    });
+
+    it('returns NPC', () => {
+      const { component } = setup();
+      component.viewerType = VIEWER_TYPE.NPC;
+      expect(component['getModelType']()).toBe(MODEL_TYPE.NPC);
+    });
+
+    it('returns null', () => {
+      const { component } = setup();
+      component.viewerType = VIEWER_TYPE.ITEM;
+      expect(component['getModelType'](4, 2)).toBeNull();
+    });
+  });
+
+  it('getItemData$', () => {
+    const { component, queryService } = setup();
+    const mockObj = of({} as any);
+    spyOn(queryService, 'query').and.returnValue(mockObj);
+    component.displayId = mockDisplayId;
+
+    expect(component['getItemData$']()).toBe(mockObj);
+    expect(queryService.query).toHaveBeenCalledOnceWith(
+      `SELECT entry, class AS _class, inventoryType FROM item_template WHERE displayid=${mockDisplayId} LIMIT 1`,
+    );
+  });
+
+  describe('verifyModelAndLoad', () => {
+    it('succeed', () => {
+      const { component, httpTestingController } = setup();
+      const mockUrl = 'www.mock.com';
+      const mockModelType = 2;
+      component.displayId = mockDisplayId;
+      spyOn<any>(component, 'getContentPathUrl').and.returnValue(mockUrl);
+      spyOn<any>(component, 'generate3Dmodel');
+      spyOn<any>(component, 'getModelType').and.returnValue(mockModelType);
+
+      component['verifyModelAndLoad']({ entry: 1, inventoryType: 2, _class: 2 });
+
+      const req = httpTestingController.expectOne(mockUrl);
+      expect(req.request.method).toEqual('GET');
+      req.flush({ tag_name: 'some newer version' });
+
+      httpTestingController.verify();
+
+      expect(component['generate3Dmodel']).toHaveBeenCalledOnceWith(mockModelType, mockDisplayId);
+      expect(component['getModelType']).toHaveBeenCalledOnceWith(2, 2);
+    });
+  });
+});
--- src/app/features/model-3d-viewer/model-3d-viewer.component.ts
+++ src/app/features/model-3d-viewer/model-3d-viewer.component.ts
@@ -1,11 +1,11 @@
-/* istanbul ignore file */
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { MysqlQueryService } from '@keira-shared/services/mysql-query.service';
 import { TableRow } from '@keira-shared/types/general';
 import * as jquery from 'jquery';
-import { BehaviorSubject, filter, Observable, Subscription } from 'rxjs';
-import { generateModels, getShadowlandDisplayId, resetModel3dElement } from './helper';
+import { BehaviorSubject, catchError, filter, Observable, of, Subscription } from 'rxjs';
+import { generateModels, getShadowlandDisplayId } from './helper';
 import { CONTENT_LIVE, CONTENT_WOTLK, MODEL_TYPE, VIEWER_TYPE } from './model-3d-viewer.model';
 
 @Component({
@@ -17,36 +17,40 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
   @Input() displayId: number;
   @Input() itemClass?: number;
   @Input() itemInventoryType?: number;
-  @Input() set modelId(modelId: number) {
-    this.displayId = modelId;
-  }
+  @Input() id? = 'model_3d';
 
   private loadedViewer$ = new BehaviorSubject<boolean>(false);
-  private subscriptions: Subscription[] = [];
+  private subscriptions = new Subscription();
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
-  constructor(private readonly sanitizer: DomSanitizer, private readonly queryService: MysqlQueryService) {
+  constructor(private readonly sanitizer: DomSanitizer, private readonly queryService: MysqlQueryService, private http: HttpClient) {
     this.setupViewer3D();
   }
 
   public itemPreview: SafeHtml = this.sanitizer.bypassSecurityTrustHtml('loading...');
 
   ngOnInit(): void {
-    resetModel3dElement();
+    this.resetModel3dElement();
     this.viewerDynamic();
   }
 
-  ngOnChanges(): void {
-    if (!!this.displayId && this.displayId > 0 && this.viewerType != null && this.viewerType != undefined) {
-      resetModel3dElement();
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes.displayId?.currentValue != changes.displayId?.previousValue &&
+      !!this.displayId &&
+      this.displayId > 0 &&
+      this.viewerType != null &&
+      this.viewerType != undefined
+    ) {
+      this.resetModel3dElement();
       this.show3Dmodel();
     }
   }
 
   show3Dmodel(): void {
     if (this.viewerType === VIEWER_TYPE.ITEM) {
-      this.subscriptions.push(
-        this.getItemData().subscribe((data) => {
+      this.subscriptions.add(
+        this.getItemData$().subscribe((data) => {
           if (data.length && 'entry' in data[0]) {
             this.verifyModelAndLoad(data[0]);
           }
@@ -57,7 +61,7 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
     }
   }
 
-  private getItemData(): Observable<TableRow[]> {
+  private getItemData$(): Observable<TableRow[]> {
     return this.queryService.query(
       `SELECT entry, class AS _class, inventoryType FROM item_template WHERE displayid=${this.displayId} LIMIT 1`,
     );
@@ -66,15 +70,26 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
   private verifyModelAndLoad({ entry, inventoryType, _class }: TableRow): void {
     const modelType = this.getModelType(_class as number, inventoryType as number);
 
-    fetch(this.getContentPathUrl(inventoryType))
-      .then(() => {
-        this.generate3Dmodel(modelType, this.displayId);
-      })
-      .catch(() => {
-        getShadowlandDisplayId(entry as number).then((displayInfo) => {
-          this.generate3Dmodel(modelType, displayInfo.displayId, CONTENT_LIVE);
-        });
-      });
+    this.subscriptions.add(
+      this.http
+        .get(this.getContentPathUrl(inventoryType))
+        .pipe(
+          catchError(
+            /* istanbul ignore next */
+            () => {
+              /* istanbul ignore next */
+              getShadowlandDisplayId(entry as number).then((displayInfo) => {
+                this.generate3Dmodel(modelType, displayInfo.displayId, CONTENT_LIVE);
+              });
+              /* istanbul ignore next */
+              return of([]);
+            },
+          ),
+        )
+        .subscribe(() => {
+          this.generate3Dmodel(modelType, this.displayId);
+        }),
+    );
   }
 
   private generate3Dmodel(
@@ -82,10 +97,10 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
     displayId: number = this.displayId,
     contentPath: string = CONTENT_WOTLK,
   ): void {
-    resetModel3dElement();
+    this.resetModel3dElement();
     generateModels(
       1,
-      `#model_3d`,
+      `#${this.id}`,
       {
         type: modelType,
         id: displayId,
@@ -122,9 +137,14 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
       return MODEL_TYPE.OBJECT;
     }
 
-    return MODEL_TYPE.NPC;
+    if (this.viewerType === VIEWER_TYPE.NPC) {
+      return MODEL_TYPE.NPC;
+    }
+
+    return null;
   }
 
+  /* istanbul ignore next */
   private setupViewer3D(): void {
     window['jQuery'] = jquery;
     window['$'] = jquery;
@@ -143,14 +163,22 @@ export class Model3DViewerComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   private viewerDynamic(): void {
-    this.subscriptions.push(
+    this.subscriptions.add(
       this.loadedViewer$.pipe(filter((loadedViewr) => loadedViewr)).subscribe(() => {
         this.show3Dmodel();
       }),
     );
   }
 
+  /* istanbul ignore next */
+  private resetModel3dElement(): void {
+    const modelElement = document.querySelector(`#${this.id}`);
+    if (modelElement) {
+      modelElement.innerHTML = '';
+    }
+  }
+
   ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
+    this.subscriptions.unsubscribe();
   }
 }
