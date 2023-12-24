595cbe3152cb3d3d3ce779f1f627297e206feb20
FrancescoBorzi <borzifrancesco@gmail.com>
2023-12-24 19:40:28 +0100

    fix: disable OnPush from sidebar

--- src/app/main/main-window/sidebar/sidebar.component.ts
+++ src/app/main/main-window/sidebar/sidebar.component.ts
@@ -19,7 +19,9 @@ import { SidebarService } from './sidebar.service';
 const animationTime = 200;
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
+  changeDetection: ChangeDetectionStrategy.Default, // TODO: migrate to OnPush: https://github.com/azerothcore/Keira3/issues/2602
+  // TODO: make the unsaved icons feature work with OnPush
   selector: 'keira-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
