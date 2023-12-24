732cad664de41b5b6626962ee6a78a49c0653dc5
FrancescoBorzi <borzifrancesco@gmail.com>
2023-12-24 20:17:47 +0100

    fix: all loots

--- src/app/shared/modules/loot-editor/loot-editor.component.ts
+++ src/app/shared/modules/loot-editor/loot-editor.component.ts
@@ -1,9 +1,12 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { MultiRowEditorService } from '@keira-abstract/service/editors/multi-row-editor.service';
 import { DTCFG } from '@keira-config/datatable.config';
 import { LOOT_MODE } from '@keira-constants/flags/loot-mode';
 import { WIKI_BASE_URL } from '@keira-constants/general';
 import { LootTemplate } from '@keira-types/loot-template.type';
+import { compareObjFn } from '@keira-shared/utils/helpers';
+import { SubscriptionHandler } from '@keira-shared/utils/subscription-handler/subscription-handler';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -11,7 +14,7 @@ import { LootTemplate } from '@keira-types/loot-template.type';
   templateUrl: './loot-editor.component.html',
   styleUrls: ['./loot-editor.component.scss'],
 })
-export class LootEditorComponent<T extends LootTemplate> {
+export class LootEditorComponent<T extends LootTemplate> extends SubscriptionHandler implements OnInit {
   @Input() editorService: MultiRowEditorService<T>;
 
   private readonly changeDetectorRef = inject(ChangeDetectorRef);
@@ -26,6 +29,22 @@ export class LootEditorComponent<T extends LootTemplate> {
 
   get referenceIds(): number[] {
     return this.editorService.newRows.filter((row) => row.Reference > 0).map((row) => row.Reference);
+  }
+
+  // TODO: this component was originally meant to avoid duplicating code
+  //  but because of it now we need this duplication. Consider refactoring the architecture.
+  /* istanbul ignore next */
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.editorService.form.valueChanges.pipe(
+        debounceTime(200),
+        distinctUntilChanged(compareObjFn),
+      ).subscribe(() => {
+        setTimeout(() => {
+          this.changeDetectorRef.detectChanges();
+        });
+      }),
+    )
   }
 
   isReference(row): boolean {
