e0bfefb6a90c561c6c595e3f4b2940f13ff0c275
Helias <stefanoborzi32@gmail.com>
2023-12-26 01:04:15 +0100

    feat: bypass cors also for binary files

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
+import * as cors from 'cors';
 import { BrowserWindow, Menu, MenuItem, app, nativeImage, shell } from 'electron';
 import * as settings from 'electron-settings';
+import * as express from 'express';
+import * as morgan from 'morgan';
 import * as path from 'path';
 import * as url from 'url';
-import { appRest } from './cors';
+
+// Inspired by https://github.com/Miorey/bypass-cors-policies
+export const appRest = express();
+const TIMEOUT = 2000;
+const SERVER_NAME = 'https://wow.zamimg.com';
+const fetch = require(`node-fetch`);
+
+appRest.use(morgan(`dev`));
+appRest.use(express.json());
+appRest.use(express.urlencoded({ extended: true }));
+appRest.use(cors());
+
+async function getFile(req) {
+  console.log(`${SERVER_NAME}${req.url}`);
+  const timeout = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error(`Request timed out after ${TIMEOUT} milli-seconds`));
+    }, TIMEOUT);
+  });
+
+  const response: any = await Promise.race([fetch(`${SERVER_NAME}${req.url}`), timeout]).catch(() => ({ ok: false, status: 408 }));
+  if (!response.ok) {
+    return response.status;
+  }
+  const buffer = await response.arrayBuffer();
+  return Buffer.from(buffer);
+}
+
+appRest.use(`/`, async (req, res, next) => {
+  const content = await getFile(req);
+  res.send(content);
+});
 
 let win;
 const args = process.argv.slice(1);
