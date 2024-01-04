adc6e2bbf8f8bd21a34c2a9a984c36cdbba6b6ed
Stefano Borzì <stefanoborzi32@gmail.com>
2024-01-04 22:22:03 +0100

    fix: creature template movement documentation link, closes #2646 (#2647)

--- src/app/features/creature/creature-template-movement/creature-template-movement.component.ts
+++ src/app/features/creature/creature-template-movement/creature-template-movement.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SingleRowEditorComponent } from '@keira-abstract/components/editors/single-row-editor.component';
-import { CreatureTemplateMovement } from '@keira-types/creature-template-movement.type';
-import { CREATURE_TEMPLATE_RESISTANCE_TABLE } from '@keira-types/creature-template-resistance.type';
+import { CREATURE_TEMPLATE_MOVEMENT_TABLE, CreatureTemplateMovement } from '@keira-types/creature-template-movement.type';
 import { CreatureHandlerService } from '../creature-handler.service';
 import { CreatureTemplateMovementService } from './creature-template-movement.service';
 
@@ -12,11 +11,14 @@ import { CreatureTemplateMovementService } from './creature-template-movement.se
 })
 export class CreatureTemplateMovementComponent extends SingleRowEditorComponent<CreatureTemplateMovement> {
   public get docUrl(): string {
-    return this.WIKI_BASE_URL + CREATURE_TEMPLATE_RESISTANCE_TABLE;
+    return this.WIKI_BASE_URL + CREATURE_TEMPLATE_MOVEMENT_TABLE;
   }
 
   /* istanbul ignore next */ // because of: https://github.com/gotwarlost/istanbul/issues/690
-  constructor(public editorService: CreatureTemplateMovementService, public handlerService: CreatureHandlerService) {
+  constructor(
+    public editorService: CreatureTemplateMovementService,
+    public handlerService: CreatureHandlerService,
+  ) {
     super(editorService, handlerService);
   }
 }
