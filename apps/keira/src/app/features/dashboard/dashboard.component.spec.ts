51bc0f6247a4ad398144b93782cb68efef682caf
Francesco Borzì <borzifrancesco@gmail.com>
2024-02-16 13:24:04 +0100

    feat(dashboard): reload button (#2745)

--- apps/keira/src/app/features/dashboard/dashboard.component.spec.ts
+++ apps/keira/src/app/features/dashboard/dashboard.component.spec.ts
@@ -20,14 +20,17 @@ class DashboardComponentPage extends PageObject<DashboardComponent> {
   get dbVersion(): HTMLTableCellElement {
     return this.query<HTMLTableCellElement>('#db-version');
   }
-  get dbWorldVersion(): HTMLTableCellElement {
-    return this.query<HTMLTableCellElement>('#db-world-version');
-  }
-  get dbWarning(): HTMLDivElement {
-    return this.query<HTMLDivElement>('#database-warning', false);
+  // get dbWorldVersion(): HTMLTableCellElement {
+  //   return this.query<HTMLTableCellElement>('#db-world-version');
+  // }
+  dbWarning(assert = true): HTMLDivElement {
+    return this.query<HTMLDivElement>('#database-warning', assert);
   }
   get commitHashUrl(): HTMLAnchorElement {
     return this.query<HTMLAnchorElement>('#commit-hash-url');
+  }
+  get reloadBtn(): HTMLButtonElement {
+    return this.query<HTMLButtonElement>('#reload-btn');
   }
 }
 
@@ -36,6 +39,12 @@ describe('DashboardComponent', () => {
     core_version: 'AzerothCore rev. 2bcedc2859e7 2019-02-17 10:04:09 +0100 (master branch) (Unix, Debug)',
     core_revision: '2bcedc2859e7',
     db_version: 'ACDB 335.3 (dev)',
+    cache_id: 3,
+  };
+  const wrongVersionRow: VersionRow = {
+    core_version: 'ShinCore rev. 2bcedc2859e7 2019-02-17 10:04:09 +0100 (master branch) (Unix, Debug)',
+    core_revision: '2bcedc2859e7',
+    db_version: 'SHINDB 335.3 (dev)',
     cache_id: 3,
   };
   const worldDbVersion = '2019_02_17_02';
@@ -66,20 +75,19 @@ describe('DashboardComponent', () => {
   };
 
   it('should correctly display the versions', () => {
-    const { fixture, page, component } = setup();
-    fixture.detectChanges();
+    const { page } = setup();
+    page.detectChanges();
 
     expect(page.coreVersion.innerHTML).toContain(versionRow.core_version);
     expect(page.coreRevision.innerHTML).toContain(versionRow.core_revision);
     expect(page.dbVersion.innerHTML).toContain(versionRow.db_version);
     expect(page.commitHashUrl.href).toEqual(`https://github.com/azerothcore/azerothcore-wotlk/commit/${versionRow.core_revision}`);
     // expect(page.dbWorldVersion.innerHTML).toContain(worldDbVersion);
-    expect(page.dbWarning).toBe(null);
-    expect(component.error).toBe(false);
+    expect(page.dbWarning(false)).toBeFalsy();
   });
 
   it('if the revision hash ends with a "+", it should be stripped in the url', () => {
-    const { fixture, page } = setup();
+    const { page } = setup();
     when(MockedMysqlQueryService.query('SELECT * FROM version')).thenReturn(
       of([
         {
@@ -89,50 +97,78 @@ describe('DashboardComponent', () => {
       ]),
     );
 
-    fixture.detectChanges();
+    page.detectChanges();
 
     expect(page.commitHashUrl.href).toEqual(`https://github.com/azerothcore/azerothcore-wotlk/commit/${versionRow.core_revision}`);
   });
 
+  describe('refresh button', () => {
+    it('when the refresh button is clicked, it should correctly reload the data', () => {
+      const { page } = setup();
+      page.detectChanges();
+      expect(page.coreVersion.innerHTML).toContain(versionRow.core_version);
+
+      const newVersion = 'A new fantastic AzerothCore version!';
+      when(MockedMysqlQueryService.query('SELECT * FROM version')).thenReturn(
+        of([
+          {
+            ...versionRow,
+            core_version: newVersion,
+          },
+        ]),
+      );
+      page.reloadBtn.click();
+      page.detectChanges();
+
+      expect(page.coreVersion.innerHTML).not.toContain(versionRow.core_version);
+      expect(page.coreVersion.innerHTML).toContain(newVersion);
+    });
+
+    it('when clicked after an error, it should clear the error out', () => {
+      const { page } = setup();
+
+      when(MockedMysqlQueryService.query(anyString())).thenReturn(of([wrongVersionRow]));
+      page.detectChanges();
+      expect(page.dbWarning()).toBeDefined();
+
+      when(MockedMysqlQueryService.query(anyString())).thenReturn(of([versionRow]));
+      page.reloadBtn.click();
+      page.detectChanges();
+
+      expect(page.dbWarning(false)).toBeFalsy();
+    });
+  });
+
   it('should correctly give error if the query does not return the data in the expected format', () => {
-    const { fixture, page, component } = setup();
+    const { page } = setup();
     when(MockedMysqlQueryService.query(anyString())).thenReturn(of([]));
     const errorSpy = spyOn(console, 'error');
 
-    fixture.detectChanges();
+    page.detectChanges();
 
     expect(errorSpy).toHaveBeenCalledTimes(1);
-    expect(page.dbWarning).toBe(null);
-    expect(component.error).toBe(false);
+    expect(page.dbWarning(false)).toBeFalsy();
   });
 
   it('should correctly give error if the query returns an error', () => {
-    const { fixture, page, component } = setup();
+    const { page } = setup();
     const error = 'some error';
     when(MockedMysqlQueryService.query(anyString())).thenReturn(throwError(error));
     const errorSpy = spyOn(console, 'error');
 
-    fixture.detectChanges();
+    page.detectChanges();
 
     expect(errorSpy).toHaveBeenCalledTimes(1);
     expect(errorSpy).toHaveBeenCalledWith(error);
-    expect(page.dbWarning).toBeDefined();
-    expect(component.error).toBe(true);
+    expect(page.dbWarning()).toBeDefined();
   });
 
   it('should correctly give error if the query returns an error', () => {
-    const { fixture, page, component } = setup();
-    const wrongVersionRow: VersionRow = {
-      core_version: 'ShinCore rev. 2bcedc2859e7 2019-02-17 10:04:09 +0100 (master branch) (Unix, Debug)',
-      core_revision: '2bcedc2859e7',
-      db_version: 'SHINDB 335.3 (dev)',
-      cache_id: 3,
-    };
+    const { page } = setup();
     when(MockedMysqlQueryService.query(anyString())).thenReturn(of([wrongVersionRow]));
 
-    fixture.detectChanges();
+    page.detectChanges();
 
-    expect(page.dbWarning).toBeDefined();
-    expect(component.error).toBe(true);
+    expect(page.dbWarning()).toBeDefined();
   });
 });
