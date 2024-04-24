6514cbf162a5870254500fe409350c09d54acb14
Instantium <nadler21+instantium@gmail.com>
2024-04-24 18:06:58 +0200

    Added SingleValueSelector to TextType and TextRange

--- libs/features/creature/src/creature-text/creature-text.component.ts
+++ libs/features/creature/src/creature-text/creature-text.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MultiRowEditorComponent } from '@keira/shared/base-abstract-classes';
-import { CreatureText } from '@keira/shared/acore-world-model';
+import { CreatureText, TEXT_TYPE, TEXT_RANGE } from '@keira/shared/acore-world-model';
 import { CreatureHandlerService } from '../creature-handler.service';
 import { CreatureTextService } from './creature-text.service';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
@@ -10,6 +10,7 @@ import { QueryOutputComponent } from '@keira/shared/base-editor-components';
 import { TranslateModule } from '@ngx-translate/core';
 import { TopBarComponent } from '@keira/shared/base-editor-components';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { SingleValueSelectorBtnComponent } from '@keira/shared/selectors';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,9 +26,13 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     ReactiveFormsModule,
     EditorButtonsComponent,
     NgxDatatableModule,
+    SingleValueSelectorBtnComponent,
   ],
 })
 export class CreatureTextComponent extends MultiRowEditorComponent<CreatureText> {
+  protected readonly TEXT_TYPE = TEXT_TYPE;
+  protected readonly TEXT_RANGE = TEXT_RANGE;
+
   public readonly editorService = inject(CreatureTextService);
   protected readonly handlerService = inject(CreatureHandlerService);
 }
