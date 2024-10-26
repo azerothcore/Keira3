a6d1bb1184f2fa452bc582cda215b0c9484f6e39
Stefano Borzì <stefanoborzi32@gmail.com>
2024-10-26 18:26:27 +0200

    feat(creature-template-model): base structure (#3079)
    * feat(creature-template-model): wip
    
    * fix: correct bad class dependency
    
    * chore: fix test
    
    * fix: all
    
    * chore(translations): add missing template_model key
    
    ---------
    
    Co-authored-by: FrancescoBorzi <borzifrancesco@gmail.com>

---
+++ libs/features/creature/src/creature-template-model/creature-template-model.service.spec.ts
@@ -0,0 +1,28 @@
+import { TestBed } from '@angular/core/testing';
+import { MysqlQueryService, SqliteService } from '@keira/shared/db-layer';
+
+import { ToastrService } from 'ngx-toastr';
+import { instance, mock } from 'ts-mockito';
+import { CreatureHandlerService } from '../creature-handler.service';
+import { SaiCreatureHandlerService } from '../sai-creature-handler.service';
+import { CreatureTemplateModelService } from './creature-template-model.service';
+
+describe('CreatureTemplateModelService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: MysqlQueryService, useValue: instance(mock(MysqlQueryService)) },
+        { provide: ToastrService, useValue: instance(mock(ToastrService)) },
+        { provide: SqliteService, useValue: instance(mock(SqliteService)) },
+        CreatureHandlerService,
+        SaiCreatureHandlerService,
+        CreatureTemplateModelService,
+      ],
+    }),
+  );
+
+  it('should be defined', () => {
+    const service: CreatureTemplateModelService = TestBed.inject(CreatureTemplateModelService);
+    expect(service).toBeDefined();
+  });
+});
---
+++ libs/shared/acore-world-model/src/entities/creature-template-model.type.ts
@@ -0,0 +1,14 @@
+import { TableRow } from '@keira/shared/constants';
+
+export const CREATURE_TEMPLATE_MODEL_TABLE = 'creature_template_model';
+export const CREATURE_TEMPLATE_MODEL_ID = 'CreatureID';
+export const CREATURE_TEMPLATE_MODEL_ID_2 = 'Idx';
+
+export class CreatureTemplateModel extends TableRow {
+  CreatureID: number = 0;
+  Idx: number = 0;
+  CreatureDisplayID: number = 0;
+  DisplayScale: number = 1;
+  Probability: number = 0;
+  VerifiedBuild: number = 0;
+}
