e53766bac531e82ba7c46bc1194b3cb0730ebd06
Stefano Borzì <stefanoborzi32@gmail.com>
2025-08-30 09:27:31 +0200

    feat(model-3d-viewer): remove proxy for cors, using wowgaming instead (#3506)
    * feat(model-3d-viewer): remove proxy for cors, using wowgaming instead
    
    * chore(package.json): remove unused packages
    
    * chore(model-3d-viewer.model.ts): remove outdated comment

--- libs/shared/model-3d-viewer/src/model-3d-viewer.model.ts
+++ libs/shared/model-3d-viewer/src/model-3d-viewer.model.ts
@@ -7,8 +7,7 @@ export const enum VIEWER_TYPE {
   OBJECT,
 }
 
-// use the local web server to make all the http calls about modelviewer to bypass CORS
-export const CONTENT_WOTLK = 'http://localhost:3003/modelviewer/wrath/';
+export const CONTENT_WOTLK = 'https://wowgaming.altervista.org/modelviewer/data/get.php?path=';
 
 export const enum Gender {
   MALE = 0,
