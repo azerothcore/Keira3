bf02d46d53bd004d96b0683ed5c8a03a18034e3d
Stefano Borzì <stefanoborzi32@gmail.com>
2026-01-17 16:19:05 +0100

    chore(ngx-translate): migrate to v17 (#3663)

--- apps/keira/src/main.ts
+++ apps/keira/src/main.ts
@@ -1,30 +1,26 @@
 import { enableProdMode, importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
 
-import { KEIRA_APP_CONFIG } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { UiSwitchModule } from 'ngx-ui-switch';
-import { ToastrModule } from 'ngx-toastr';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { withInterceptorsFromDi, provideHttpClient, HttpClient } from '@angular/common/http';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { KEIRA_APP_CONFIG_TOKEN, highlightOptions, toastrConfig, uiSwitchConfig } from '@keira/shared/config';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { KEIRA_ROUTES } from './app/routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { KEIRA_APP_CONFIG_TOKEN, highlightOptions, toastrConfig, uiSwitchConfig } from '@keira/shared/config';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { ToastrModule } from 'ngx-toastr';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { AppComponent } from './app/app.component';
+import { KEIRA_ROUTES } from './app/routes';
+import { KEIRA_APP_CONFIG } from './environments/environment';
 
 if (KEIRA_APP_CONFIG.production) {
   enableProdMode();
-}
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 bootstrapApplication(AppComponent, {
@@ -43,15 +39,11 @@ bootstrapApplication(AppComponent, {
       TooltipModule.forRoot(),
       ToastrModule.forRoot(toastrConfig),
       UiSwitchModule.forRoot(uiSwitchConfig),
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: createTranslateLoader,
-          deps: [HttpClient],
-        },
-        defaultLanguage: 'en',
-      }),
     ),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
+      fallbackLang: 'en',
+    }),
     /* Config */
     provideRouter(KEIRA_ROUTES, withHashLocation()),
     { provide: KEIRA_APP_CONFIG_TOKEN, useValue: KEIRA_APP_CONFIG },
