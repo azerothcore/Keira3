9bdc6709db2e82e5e7b6040670a71d06e859d308
sogla <sogladev@gmail.com>
2026-03-05 19:15:14 +0100

    fix: lint

--- libs/shared/base-editor-components/src/create/create.component.spec.ts
+++ libs/shared/base-editor-components/src/create/create.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
