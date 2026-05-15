a4cdb5b12506e8388c25181fe2ab77139429eebe
ggro <hensongeronimo@gmail.com>
2026-05-15 18:30:29 -0300

    add translation for button text

--- libs/shared/base-editor-components/src/query-output/query-error/query-error.component.ts
+++ libs/shared/base-editor-components/src/query-output/query-error/query-error.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { QueryError } from 'mysql2';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'keira-query-error',
   templateUrl: './query-error.component.html',
   styleUrls: ['./query-error.component.scss'],
-  imports: [],
+  imports: [TranslateModule],
 })
 export class QueryErrorComponent {
   @Input() error: QueryError | undefined;
