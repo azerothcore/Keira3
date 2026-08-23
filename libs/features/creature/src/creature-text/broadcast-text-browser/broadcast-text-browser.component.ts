8b6c6001bf588939070f020adb0637525e995466
Helias <stefanoborzi32@gmail.com>
2026-08-23 13:25:09 +0200

    chore(broadcast-text-browser): remove useless module FormsModule

--- libs/features/creature/src/creature-text/broadcast-text-browser/broadcast-text-browser.component.ts
+++ libs/features/creature/src/creature-text/broadcast-text-browser/broadcast-text-browser.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, input, output } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HighlightjsWrapperComponent } from '@keira/shared/base-editor-components';
 import { DTCFG } from '@keira/shared/config';
 import { TranslatePipe } from '@ngx-translate/core';
 import { NgxDatatableModule } from '@siemens/ngx-datatable';
 import { TooltipDirective } from 'ngx-bootstrap/tooltip';
-import { BroadcastTextBrowserService } from './broadcast-text-browser.service';
 import { BroadcastTextRow } from './broadcast-text-browser.model';
+import { BroadcastTextBrowserService } from './broadcast-text-browser.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'keira-broadcast-text-browser',
   templateUrl: './broadcast-text-browser.component.html',
-  imports: [FormsModule, ReactiveFormsModule, HighlightjsWrapperComponent, NgxDatatableModule, TranslatePipe, TooltipDirective],
+  imports: [ReactiveFormsModule, HighlightjsWrapperComponent, NgxDatatableModule, TranslatePipe, TooltipDirective],
 })
 export class BroadcastTextBrowserComponent {
   /** Whether the editor has a row to copy into: without one there is nowhere for the copy to land. */
